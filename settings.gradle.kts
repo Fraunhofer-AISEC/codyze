67d2d81d68b951d8a5ec1e9a4224cd6440e14346
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2019-03-06 22:12:17 +0100

    fix(build): Add deployment credentials

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,4 @@
 rootProject.name = "cpganalysisserver"
+
+val deployUsername: String by settings // imported by gradle.properties (set via CI, not in git!)
+val deployPassword: String by settings // imported by gradle.properties (set via CI, not in git!)

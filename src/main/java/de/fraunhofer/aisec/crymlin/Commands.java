a97d8ae49f055b20f6ba2c0d3acc4669bdbd3492
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2019-03-15 15:36:05 +0100

    feature(console): Commands for Jython console. Source files can be loaded and analyzed via console command.

---
+++ src/main/java/de/fraunhofer/aisec/crymlin/Commands.java
@@ -0,0 +1,80 @@
+package de.fraunhofer.aisec.crymlin;
+
+import de.fraunhofer.aisec.cpg.AnalysisConfiguration;
+import de.fraunhofer.aisec.cpg.AnalysisManager;
+import de.fraunhofer.aisec.crymlin.passes.StatementsPerMethodPass;
+import de.fraunhofer.aisec.crymlin.server.AnalysisContext;
+import de.fraunhofer.aisec.crymlin.server.AnalysisServer;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * These commands are only used by the Jython console.
+ *
+ * @author julian
+ */
+public class Commands {
+
+  /**
+   * Starts the analysis of a single file or all files in a directory.
+   *
+   * @param url
+   */
+  public void analyze(String url) {
+    List<File> files = new ArrayList<>();
+    File f = new File(url);
+    if (f.isDirectory()) {
+      files.addAll(Arrays.asList(f.listFiles()));
+    } else {
+      files.add(f);
+    }
+
+    AnalysisContext ctx = new AnalysisContext();
+    AnalysisManager analyzer =
+        AnalysisManager.builder()
+            .config(
+                AnalysisConfiguration.builder()
+                    .sourceFiles(files.toArray(new File[0]))
+                    .registerPass(new StatementsPerMethodPass(ctx))
+                    .build())
+            .build();
+
+    AnalysisServer.getInstance().analyze(analyzer);
+  }
+
+  /** Prints help to stdout. */
+  public void help() {
+    System.out.println(
+        "Use the \"server\" object to control the analysis server.\n"
+            + "\n"
+            + "   server.analyze(\"src/test/resources/good/Bouncycastle.java\")\n"
+            + "          Analyze a single source file.\n"
+            + "\n"
+            + "   server.analyze(\"src/test/resources/good\")\n"
+            + "          Analyze all source files in a directory \n"
+            + "\n"
+            + "\n"
+            + "You may then start writing crymlin queries using the \"crymlin\" object.\n"
+            + "\n"
+            + "Examples: \n"
+            + "   crymlin.recorddeclarations().toList()\n"
+            + "          Returns array of vertices representing RecordDeclarations.\n"
+            + "\n"
+            + "   crymlin.recorddeclaration(\"good.Bouncycastle\").next()\n"
+            + "          Returns vertex representing the RecordDeclarations of \"good.Bouncycastle\".\n"
+            + "\n"
+            + "   crymlin.recorddeclaration(\"good.Bouncycastle\").sourcecode().next()\n"
+            + "          Returns source code of \"good.Bouncycastle\".\n"
+            + "\n"
+            + "   crymlin.translationunits().name().toList()\n"
+            + "          Returns array of strings representing the names of TranslationUnits.\n"
+            + "\n"
+            + "   crymlin.translationunits().next()\n"
+            + "          Returns the first TranslationUnit vertex (or null if none exists).\n"
+            + "\n"
+            + "   dir(crymlin.translationunits())\n"
+            + "          Good ol' Python dir() to find out what properties/methods are available.\n");
+  }
+}
--- src/main/java/de/fraunhofer/aisec/crymlin/Main.java
+++ src/main/java/de/fraunhofer/aisec/crymlin/Main.java
@@ -2,16 +2,24 @@ package de.fraunhofer.aisec.crymlin;
 
 import de.fraunhofer.aisec.crymlin.server.AnalysisServer;
 import de.fraunhofer.aisec.crymlin.server.ServerConfiguration;
+import java.util.logging.LogManager;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 /** Start point of the standalone analysis server. */
 public class Main {
+
+  static {
+    // bridge java.util.logging to slf4j
+    LogManager.getLogManager().reset();
+    SLF4JBridgeHandler.install();
+  }
 
   public static void main(String... args) throws Exception {
 
     System.out.println("Analysis server starting ...");
     AnalysisServer server =
         AnalysisServer.builder()
-            .config(ServerConfiguration.builder().launchConsole(true).build())
+            .config(ServerConfiguration.builder().launchLsp(true).launchConsole(true).build())
             .build();
 
     server.start();

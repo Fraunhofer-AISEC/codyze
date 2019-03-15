7c662d5dd203b7ab20d4e874077443d893061c05
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2019-03-15 09:30:13 +0100

    fix(format): Spotless formatting

--- src/main/java/de/fraunhofer/aisec/crymlin/utils/Utils.java
+++ src/main/java/de/fraunhofer/aisec/crymlin/utils/Utils.java
@@ -6,13 +6,14 @@ import de.fraunhofer.aisec.cpg.graph.RecordDeclaration;
 public class Utils {
 
   /**
-   * Returns the fully qualified signature of a method within a record declaration (e.g., a Java class).
-   * 
+   * Returns the fully qualified signature of a method within a record declaration (e.g., a Java
+   * class).
+   *
    * @param r
    * @param m
    * @return
    */
   public static String toFullyQualifiedSignature(RecordDeclaration r, MethodDeclaration m) {
-	  return r.getName() + "." + m.getSignature();
+    return r.getName() + "." + m.getSignature();
   }
 }

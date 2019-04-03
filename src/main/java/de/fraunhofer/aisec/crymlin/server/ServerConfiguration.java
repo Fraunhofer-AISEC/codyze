18e75245f1ae96da70954038021bdf9135ec509f
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2019-04-03 11:45:48 +0200

    feature(serverconfig): Allow setting Neo4J credentials in cpgserver

--- src/main/java/de/fraunhofer/aisec/crymlin/server/ServerConfiguration.java
+++ src/main/java/de/fraunhofer/aisec/crymlin/server/ServerConfiguration.java
@@ -9,9 +9,22 @@ public class ServerConfiguration {
   /** Should the server launch an LSP server? */
   public final boolean launchLsp;
 
-  private ServerConfiguration(boolean launchConsole, boolean launchLsp) {
+  /** Neo4J URI. */
+  public String dbUri;
+
+  /** Neo4J username. */
+  public String dbUser;
+
+  /** Neo4J password. */
+  public String dbPassword;
+
+  private ServerConfiguration(
+      boolean launchConsole, boolean launchLsp, String dbUri, String dbUser, String dbPassword) {
     this.launchConsole = launchConsole;
     this.launchLsp = launchLsp;
+    this.dbUri = dbUri;
+    this.dbUser = dbUser;
+    this.dbPassword = dbPassword;
   }
 
   public static Builder builder() {
@@ -21,6 +34,9 @@ public class ServerConfiguration {
   public static class Builder {
     private boolean launchConsole = true;
     private boolean launchLsp = true;
+    private String dbUri = "bolt://localhost";
+    private String dbUser = "neo4j";
+    private String dbPassword = "password";
 
     public Builder launchConsole(boolean launchConsole) {
       this.launchConsole = launchConsole;
@@ -32,8 +48,23 @@ public class ServerConfiguration {
       return this;
     }
 
+    public Builder dbUri(String dbUri) {
+      this.dbUri = dbUri;
+      return this;
+    }
+
+    public Builder dbUser(String dbUser) {
+      this.dbUser = dbUser;
+      return this;
+    }
+
+    public Builder dbPassword(String dbPassword) {
+      this.dbPassword = dbPassword;
+      return this;
+    }
+
     public ServerConfiguration build() {
-      return new ServerConfiguration(launchConsole, launchLsp);
+      return new ServerConfiguration(launchConsole, launchLsp, dbUri, dbUser, dbPassword);
     }
   }
 }

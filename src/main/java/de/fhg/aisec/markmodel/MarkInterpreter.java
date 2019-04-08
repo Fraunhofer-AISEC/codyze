915a3919291ac1523d79658a769b247412fac68b
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2019-04-08 22:00:56 +0200

    update(MARK): Parsing of MARK works *very* quick n dirty for milestone 1. This needs a major re-write to drive a proper typestate analysis.
    Tutorial:
    
    - Start the cpganalysisserver console
    - Paste: server.load_rules("../mark-crymlin-eclipse-plugin/examples/PoC_MS1/Botan_CipherMode.mark")
      (MARK model is loaded)
    - Paste: server.analyze("src/test/resources/symm_block_cipher.cpp")
      (Source file is analyzed)
    - Paste: server.list_rules()
       (4 rules will be listed)
    - Paste: server.show_findings()
      -> "Rule SimpleUseOfBotan_CipherMode is satisfied"

--- src/main/java/de/fhg/aisec/markmodel/MarkInterpreter.java
+++ src/main/java/de/fhg/aisec/markmodel/MarkInterpreter.java
@@ -1,5 +1,12 @@
 package de.fhg.aisec.markmodel;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.fhg.aisec.mark.markDsl.Argument;
 import de.fhg.aisec.mark.markDsl.ComparisonExpression;
 import de.fhg.aisec.mark.markDsl.Expression;
@@ -9,10 +16,13 @@ import de.fhg.aisec.mark.markDsl.LiteralListExpression;
 import de.fhg.aisec.mark.markDsl.LogicalAndExpression;
 import de.fhg.aisec.mark.markDsl.LogicalOrExpression;
 import de.fhg.aisec.mark.markDsl.Operand;
-import java.util.stream.Collectors;
-import org.checkerframework.checker.nullness.qual.NonNull;
+import de.fhg.aisec.mark.markDsl.OrderExpression;
+import de.fhg.aisec.mark.markDsl.SequenceExpression;
+import de.fhg.aisec.mark.markDsl.Terminal;
+import de.fraunhofer.aisec.crymlin.server.AnalysisServer;
 
 public class MarkInterpreter {
+  private static final Logger log = LoggerFactory.getLogger(AnalysisServer.class);
   @NonNull private Mark markModel;
 
   public MarkInterpreter(@NonNull Mark markModel) {
@@ -29,9 +39,9 @@ public class MarkInterpreter {
           + " || "
           + exprToString(((LogicalOrExpression) expr).getRight());
     } else if (expr instanceof LogicalAndExpression) {
-      return exprToString(((LogicalOrExpression) expr).getLeft())
+      return exprToString(((LogicalAndExpression) expr).getLeft())
           + " && "
-          + exprToString(((LogicalOrExpression) expr).getRight());
+          + exprToString(((LogicalAndExpression) expr).getRight());
     } else if (expr instanceof ComparisonExpression) {
       ComparisonExpression compExpr = (ComparisonExpression) expr;
       return exprToString(compExpr.getLeft())
@@ -61,11 +71,87 @@ public class MarkInterpreter {
       return ((Operand) expr).getOperand();
     } else if (expr instanceof Literal) {
       return ((Literal) expr).getValue();
+    } else if (expr instanceof SequenceExpression) {
+      SequenceExpression seq = ((SequenceExpression) expr);
+      return exprToString(seq.getLeft()) + " => " + exprToString(seq.getRight());
+    } else if (expr instanceof Terminal) {
+      return ((Terminal) expr).getOp();
+    } else if (expr instanceof OrderExpression) {
+      OrderExpression order = (OrderExpression) expr;
+      SequenceExpression seq = (SequenceExpression) order.getExp();
+      if (seq != null) {
+        return exprToString(seq);
+      } else {
+        return order.toString();
+      }
     }
     return expr.toString();
   }
 
   private String argToString(Argument arg) {
     return exprToString((Expression) arg); // Every Argument is also an Expression
+  }
+
+  /**
+   * DUMMY. JUST FOR DEMO. REWRITE.
+   * 
+   * Evaluates a MARK rule against the results of the analysis.
+   * @param r
+   * @return
+   */
+  public boolean evaluateRule(MRule r) {
+    // TODO parse rule and do something with it
+    Optional<String> matchingEntity =
+        r.getStatement().getEntities().stream()
+            .map(ent -> ent.getE().getName())
+            .filter(entityName -> this.markModel.getPopulatedEntities().containsKey(entityName))
+            .findAny();
+    if (matchingEntity.isPresent()) {
+      System.out.println("Found matching entity " + matchingEntity.get());
+      Expression ensureExpr = r.getStatement().getEnsure().getExp();
+      log.debug(exprToString(ensureExpr));
+      // TODO evaluate expression against populated mark entities
+      if (evaluateExpr(ensureExpr)) {
+        log.debug("Rule {} is satisfied.", r.getName());
+        return true;
+      } else {
+        log.debug("Rule {} is matched but violated.", r.getName());
+        return false;
+      }
+    }
+    return false;
+  }
+
+  private boolean evaluateExpr(Expression expr) {
+    if (expr instanceof SequenceExpression) {
+      OrderExpression left = ((SequenceExpression) expr).getLeft();
+      OrderExpression right = ((SequenceExpression) expr).getRight();
+      return evaluateExpr(left) && evaluateExpr(right);
+    } else if (expr instanceof Terminal) {
+      return containedInModel((Terminal) expr);
+    } else if (expr instanceof OrderExpression) {      
+      SequenceExpression seqxpr = (SequenceExpression) ((OrderExpression) expr).getExp();
+      if (seqxpr != null) {
+        return evaluateExpr(seqxpr);
+      }
+    } else {
+      System.out.println("Cannot evaluate " + expr.getClass());
+    }
+    return false;
+  }
+
+  /**
+   * DUMMY FOR DEMO.
+   * 
+   * Method fakes that a statement is contained in the a MARK entity
+   * 
+   * @param orderExpression
+   * @param orderExpression2
+   * @return
+   */
+  private boolean containedInModel(Terminal expr) {
+    System.out.println(exprToString(expr));
+    
+    return true;
   }
 }

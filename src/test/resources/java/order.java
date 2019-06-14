c970946b412f96a3e06dc1269fa90b56df7894b9
Dennis Titze <dennis.titze@aisec.fraunhofer.de>
2019-06-14 08:32:38 +0200

    one more order test, bugfix for order start evaluation, possible optimization (deactivated for now)

--- src/test/resources/java/order.java
+++ src/test/resources/java/order.java
@@ -2,57 +2,46 @@ public class Someclass {
 // DOES NOT COMPILE
 // DOES NOT MAKE REAL SENSE
 
-    void do_crypt()
-    {
+  char[] cipher;
+  SymmetricKey key;
+  InitializationVector iv;
+  Cipher_Dir direction;
+  char[] buf;
 
-      char[] cipher;
-      SymmetricKey key;
-      InitializationVector iv;
-      Cipher_Dir direction;
-      char[] buf;
-
-      Botan p = new Botan(1);
-      p.set_key(key);
-      p.start(iv.bits_of());
-      p.finish(buf);
-      p.foo(); // not in the entity and therefore ignored
-      p.set_key(key);
-
-      Botan p2 = new Botan(2);
-      p2.start(iv.bits_of());
-      // missing p2.finish(buf);
-
-      Botan p3 = new Botan(2);
-      if (3 < 4) {
-        p3.start(iv);
-      }
-      p3.finish(buf);
-      // potentially wrong path which only calls p3.finish without p3.start
-
-      // ok:
-      Botan p4 = new Botan(2);
+  void nok1() {
+    Botan p = new Botan(1);
+    p.set_key(key); // not allowed as start
+    p.start(iv.bits_of());
+    p.finish(buf);
+    p.foo(); // not in the entity and therefore ignored
+    p.set_key(key);
+  }
+  void nok2 () {
+    Botan p2 = new Botan(2);
+    p2.start(iv.bits_of());
+    // missing p2.finish(buf);
+  }
+  void nok3 () {
+    Botan p3 = new Botan(2);
+    if (3 < 4) {
+      p3.start(iv);
+    }
+    p3.finish(buf);
+    // potentially wrong path which only calls p3.finish without p3.start
+  }
+  void ok() {
+    // ok:
+    Botan p4 = new Botan(2);
+    p4.start(iv);
+    p4.finish(buf);
+  }
+  void nok4 () {
+    Botan p4 = new Botan(2);
+    if (true) {
       p4.start(iv);
       p4.finish(buf);
-
     }
+    p4.start(iv); // not ok, p4 is already finished
+    p4.finish(buf);
+  }
 }
-/**
- op start() {
-   Botan::start(iv);
-
-   forbidden Botan::start();
-   forbidden Botan::start(_);
-   forbidden Botan::start(nonce, _);
-   forbidden Botan::start_msg(*);
- }
- op finish() {
-   Botan::finish(_)
- }
-
- rule UseOfBotan_CipherMode {
-   using Forbidden as cm
-   ensure
-    order cm.start(), cm.finish()
-   onfail WrongUseOfBotan_CipherMode
- }
-*/

6d29d02b1d974694ca13ab123e79bc93f10d19de
Dennis Titze <dennis.titze@aisec.fraunhofer.de>
2019-07-26 13:27:25 +0200

    correctly use the connectionto neo4j in Traversals

---
+++ src/main/java/de/fraunhofer/aisec/crymlin/connectors/db/DriverSingleton.java
@@ -0,0 +1,33 @@
+package de.fraunhofer.aisec.crymlin.connectors.db;
+
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+
+public class DriverSingleton {
+
+  private static Driver driver;
+  private static final Object sync = new Object();
+
+  public static Driver getInstance() {
+    synchronized (sync) {
+      if (driver == null) {
+        String uri = System.getenv().getOrDefault("NEO4J_URI", "bolt://localhost");
+        String username = System.getenv().getOrDefault("NEO4J_USERNAME", "neo4j");
+        String password = System.getenv().getOrDefault("NEO4J_PASSWORD", "password");
+
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password));
+      }
+      return driver;
+    }
+  }
+
+  public static synchronized void destroy() {
+    synchronized (sync) {
+      if (driver != null) {
+        driver.close();
+        driver = null;
+      }
+    }
+  }
+}

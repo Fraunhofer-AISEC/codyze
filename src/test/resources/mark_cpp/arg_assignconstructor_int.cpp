feaa97a6eab506377f417e669e17ff86ee913646
Julian Schuette <julian.schuette@gmx.net>
2019-10-18 21:08:38 +0200

    Added explaining comments to the failing test examples committed by Florian.

--- src/test/resources/mark_cpp/arg_assignconstructor_int.cpp
+++ src/test/resources/mark_cpp/arg_assignconstructor_int.cpp
@@ -3,6 +3,10 @@ int call(int a) {
   return a + 1;
 }
 
+/*
+ foo is assigned the value 42 by "constructor initialization". The ExpressionEvaluator of Mark
+ should be able to resolve this.
+*/
 int main() {
   int foo(42);
   call(foo);

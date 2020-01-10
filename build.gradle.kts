6c8a964a7b0fa3ae025784a9eb898a5ca0899d97
Dennis Titze <dennis.titze@aisec.fraunhofer.de>
2020-01-10 10:09:40 +0100

    fixed default_jvm_opts

--- build.gradle.kts
+++ build.gradle.kts
@@ -170,7 +170,7 @@ dependencies {
 application {
     mainClassName = "de.fraunhofer.aisec.analysis.Main"
     // Required to give Ehcache deep reflective access to fields to correctly esitmate the cache size.
-    applicationDefaultJvmArgs = listOf("--add-opens java.base/jdk.internal.reflect=ALL-UNNAMED")
+    applicationDefaultJvmArgs = listOf("--add-opens=java.base/jdk.internal.reflect=ALL-UNNAMED")
 }
 tasks.named<Test>("test") {
     useJUnitPlatform()

6039e5bae971e72d969f807e9e43a18ca8919f7e
Julian Schuette <julian.schuette@gmx.net>
2020-01-17 22:59:04 +0100

    Fix: Publish script

--- publish_to_gh_pages.sh
+++ publish_to_gh_pages.sh
@@ -13,16 +13,16 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+git worktree add -B gh-pages public github/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo -c docs/
+hugo -s ./docs/ -d ../public
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages"
 
-#echo "Now do 'git push --all' to publish Github page"
+echo "Now do 'git push --all' to publish Github page"
 #git push --all

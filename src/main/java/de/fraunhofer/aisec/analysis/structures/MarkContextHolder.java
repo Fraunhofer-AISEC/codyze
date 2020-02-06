b28fbebb3506e702e1eb8167b551812234192873
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2020-02-06 23:20:41 +0100

    Style: Method name according to conventions

--- src/main/java/de/fraunhofer/aisec/analysis/structures/MarkContextHolder.java
+++ src/main/java/de/fraunhofer/aisec/analysis/structures/MarkContextHolder.java
@@ -135,7 +135,7 @@ public class MarkContextHolder {
 		this.createFindingsDuringEvaluation = b;
 	}
 
-	public boolean createFindingsDuringEvaluation() {
+	public boolean isCreateFindingsDuringEvaluation() {
 		return createFindingsDuringEvaluation;
 	}
 }

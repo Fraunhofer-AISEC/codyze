fb184f0d542f3f29be2dbfa6302835c3b4dda2f1
Oliver Braunsdorf <oliver.braunsdorf@aisec.fraunhofer.de>
2020-03-06 09:36:40 +0100

    forgotten files

--- src/test/resources/botan_rule_tr_test/5_3_02.cpp
+++ src/test/resources/botan_rule_tr_test/5_3_02.cpp
@@ -6,8 +6,6 @@ int main()
    {
    Botan::AutoSeeded_RNG rng;
    const std::vector<uint8_t> key = Botan::SymmetricKey(rng, 16);
-   uint32
-   uint8_t iv[iv_len],
    const std::vector<uint8_t>s iv = Botan::hex_decode("FFFFFFFFFFFFFFFFFFFFFFFF");
    const std::vector<uint8_t> data = Botan::hex_decode("6BC1BEE22E409F96E93D7E117393172A");
    Botan::MessageAuthenticationCode mac = Botan::MessageAuthenticationCode::create("GMAC(AES-256)");

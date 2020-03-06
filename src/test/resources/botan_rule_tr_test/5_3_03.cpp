dc39bb57291afdacca3bdebd0359e506d837be88
Oliver Braunsdorf <oliver.braunsdorf@aisec.fraunhofer.de>
2020-03-06 13:15:40 +0100

    more test cases for Botan TR rules

---
+++ src/test/resources/botan_rule_tr_test/5_3_03.cpp
@@ -0,0 +1,20 @@
+#include <botan/mac.h>
+#include <botan/hex.h>
+#include <iostream>
+
+int main()
+   {
+   Botan::AutoSeeded_RNG rng;
+   const std::vector<uint8_t> key = Botan::SymmetricKey(rng, 16);
+   size_t iv_len = 12;
+   uint8_t iv[iv_len]  = {0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63, 0x63};
+   const std::vector<uint8_t> data = Botan::hex_decode("6BC1BEE22E409F96E93D7E117393172A");
+   Botan::MessageAuthenticationCode mac = Botan::MessageAuthenticationCode::create("GMAC(AES-256)");
+   if(!mac)
+      return 1;
+   mac->set_key(key);
+   mac->start(iv, iv_len);
+   mac->update(data);
+   Botan::secure_vector<uint8_t> tag = mac->final();
+   std::cout << mac->name() << ": " << Botan::hex_encode(tag) << std::endl;
+   }

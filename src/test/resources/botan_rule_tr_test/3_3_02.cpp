6bd596953607e00f978aaa14a78fdd3cc313aad4
Dennis Titze <dennis.titze@aisec.fraunhofer.de>
2020-03-06 15:44:53 +0100

    CPP-MARK Unittests++, fixed a bug in the NFA-Order for Callexpressions inside ConstructExpressions

--- src/test/resources/botan_rule_tr_test/3_3_02.cpp
+++ src/test/resources/botan_rule_tr_test/3_3_02.cpp
@@ -10,18 +10,18 @@ int main (int argc, char* argv[]) {
      return 1;
   std::string plaintext("Your great-grandfather gave this watch to your granddad for good luck. Unfortunately, Dane's luck wasn't as good as his old man's.");
   std::vector<uint8_t> pt(plaintext.data(),plaintext.data()+plaintext.length());
-  std::unique_ptr<Botan::RandomNumberGenerator> rng(new Botan::AutoSeeded_RNG);
+  Botan::AutoSeeded_RNG rng();
 
   //load keypair
-  std::unique_ptr<Botan::Private_Key> kp(Botan::PKCS8::load_key(argv[1],*rng.get()));
+  Botan::Private_Key kp(Botan::PKCS8::load_key(argv[1], rng));
 
   Botan::EC_Group ec_group("brainpoolP256r1");
 
   Botan::ECIES_System_Params ecies_params(ec_group, "KDF2(SHA-256)", "AES-256", 32, "HMAC", 16);
 
   //encrypt with pk
-  Botan::ECIES_Encryptor enc(*rng.get(), ecies_params);
-  std::vector<uint8_t> ct = enc.encrypt(pt,*rng.get());
+  Botan::ECIES_Encryptor enc( rng, ecies_params);
+  std::vector<uint8_t> ct = enc.encrypt(pt, rng);
 
   return 0;
  }

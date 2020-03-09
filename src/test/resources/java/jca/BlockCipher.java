33ab55c987a5d339a524082d2b9c2411a35f1fe2
Wendland, Florian <florian.wendland@aisec.fraunhofer.de>
2020-03-09 09:22:20 +0100

    More test files and updated test case

--- src/test/resources/java/jca/BlockCipher.java
+++ src/test/resources/java/jca/BlockCipher.java
@@ -1,83 +1,49 @@
-import java.security.InvalidKeyException;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
-import javax.crypto.BadPaddingException;
+import java.security.NoSuchProviderException;
+
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
-public class BlockCipher {
+public class BlockCipher extends BCBase {
 
-  public byte[] encryptAES(Key k, byte[] plaintext)
-      throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-          IllegalBlockSizeException, BadPaddingException {
-    Cipher c = Cipher.getInstance("AES");
-    c.init(Cipher.ENCRYPT_MODE, k);
-    c.update(plaintext);
-    return c.doFinal();
-  }
+	public Cipher getAES() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException {
+		return Cipher.getInstance("AES", "BC");
+	}
 
-  public byte[] encryptAES_128(Key k, byte[] plaintext)
-      throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-          IllegalBlockSizeException, BadPaddingException {
-    Cipher c = Cipher.getInstance("AES_128");
-    c.init(Cipher.ENCRYPT_MODE, k);
-    c.update(plaintext);
-    return c.doFinal();
-  }
+	public Cipher getDES() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException {
+		return Cipher.getInstance("DES", "BC");
+	}
 
-  public byte[] encryptAES_192(Key k, byte[] plaintext)
-      throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-          IllegalBlockSizeException, BadPaddingException {
-    Cipher c = Cipher.getInstance("AES_192");
-    c.init(Cipher.ENCRYPT_MODE, k);
-    c.update(plaintext);
-    return c.doFinal();
-  }
+	public Cipher getSerpent() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException {
+		return Cipher.getInstance("Serpent", "BC");
+	}
 
-  public byte[] encryptAES_256(Key k, byte[] plaintext)
-      throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-          IllegalBlockSizeException, BadPaddingException {
-    Cipher c = Cipher.getInstance("AES_256");
-    c.init(Cipher.ENCRYPT_MODE, k);
-    c.update(plaintext);
-    return c.doFinal();
-  }
+	public Cipher getTwofish() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException {
+		return Cipher.getInstance("Twofish", "BC");
+	}
 
-  public byte[] encryptDES(Key k, byte[] plaintext)
-      throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-          IllegalBlockSizeException, BadPaddingException {
-    Cipher c = Cipher.getInstance("DES");
-    c.init(Cipher.ENCRYPT_MODE, k);
-    c.update(plaintext);
-    return c.doFinal();
-  }
-
-  public byte[] encryptDESede(Key k, byte[] plaintext)
-      throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-          IllegalBlockSizeException, BadPaddingException {
-    Cipher c = Cipher.getInstance("DESede");
-    c.init(Cipher.ENCRYPT_MODE, k);
-    c.update(plaintext);
-    return c.doFinal();
-  }
-
-  public byte[] encryptBlowfish(Key k, byte[] plaintext)
-      throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-          IllegalBlockSizeException, BadPaddingException {
-    Cipher c = Cipher.getInstance("Blowfish");
-    c.init(Cipher.ENCRYPT_MODE, k);
-    c.update(plaintext);
-    return c.doFinal();
-  }
-
-  public byte[] encryptRC2(Key k, byte[] plaintext)
-      throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-          IllegalBlockSizeException, BadPaddingException {
-    Cipher c = Cipher.getInstance("RC2");
-    c.init(Cipher.ENCRYPT_MODE, k);
-    c.update(plaintext);
-    return c.doFinal();
-  }
-  
+	public Cipher getAES_128() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException {
+		return Cipher.getInstance("AES_128", "BC"); // no such algorithm
+	}
+	
+	/*
+	 * for testing
+	 */
+	public static void main(String[] args) throws Exception {
+		BlockCipher bc = new BlockCipher();
+		
+		bc.getAES();
+		bc.getDES();
+		bc.getSerpent();
+		bc.getTwofish();
+		
+		try {
+			bc.getAES_128(); // NoSuchAlgorithmException
+		} catch (NoSuchAlgorithmException nsae) {
+			System.err.println(nsae);
+		}
+		
+		System.out.println("Done");
+	}
+	
 }

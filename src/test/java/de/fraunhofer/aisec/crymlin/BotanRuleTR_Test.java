0d8584674e2f667778a9eb031be8173e611af548
Oliver Braunsdorf <oliver.braunsdorf@aisec.fraunhofer.de>
2020-03-09 18:13:52 +0100

    spotless

--- src/test/java/de/fraunhofer/aisec/crymlin/BotanRuleTR_Test.java
+++ src/test/java/de/fraunhofer/aisec/crymlin/BotanRuleTR_Test.java
@@ -156,7 +156,7 @@ public class BotanRuleTR_Test extends AbstractMarkTest {
 		Set<Finding> findings = performTest("botan_rule_tr_test/3_4_01.cpp", "dist/mark/botan/");
 		expected(findings,
 			"line 28: Violation against Order: Base mac is not correctly terminated. Expected one of [m.init] to follow the correct last call on this base. (MACOrder)",
-			"line 25: MarkRuleEvaluationFinding: Rule _3_4_01_DLIES_KDF verified",  // ok
+			"line 25: MarkRuleEvaluationFinding: Rule _3_4_01_DLIES_KDF verified", // ok
 			"line 17: MarkRuleEvaluationFinding: Rule _3_4_02_DLIES_KEYLEN_2022 verified",
 			"line 20: Verified Order: PubKeyOrder",
 			"line 19: Verified Order: PrivKeyOrder",
@@ -168,12 +168,12 @@ public class BotanRuleTR_Test extends AbstractMarkTest {
 	public void test_rule_3_4_02() throws Exception {
 		Set<Finding> findings = performTest("botan_rule_tr_test/3_4_02.cpp", "dist/mark/botan/");
 		expected(findings,
-			"line 17: MarkRuleEvaluationFinding: Rule _3_4_02_DLIES_KEYLEN_2022 verified",  // ok
-			"line 19: Verified Order: PrivKeyOrder",  // ok
-			"line 17: MarkRuleEvaluationFinding: Rule _3_4_02_DLIES_KEYLEN violated",  // expected verified
-			"line 27: MarkRuleEvaluationFinding: Rule _5_3_01_MAC verified",  // ok
-			"line 27: Violation against Order: Base mac is not correctly terminated. Expected one of [m.init] to follow the correct last call on this base. (MACOrder)",  // look at this again
-			"line 24: MarkRuleEvaluationFinding: Rule _3_4_01_DLIES_KDF verified"  // ok
+			"line 17: MarkRuleEvaluationFinding: Rule _3_4_02_DLIES_KEYLEN_2022 verified", // ok
+			"line 19: Verified Order: PrivKeyOrder", // ok
+			"line 17: MarkRuleEvaluationFinding: Rule _3_4_02_DLIES_KEYLEN violated", // expected verified
+			"line 27: MarkRuleEvaluationFinding: Rule _5_3_01_MAC verified", // ok
+			"line 27: Violation against Order: Base mac is not correctly terminated. Expected one of [m.init] to follow the correct last call on this base. (MACOrder)", // look at this again
+			"line 24: MarkRuleEvaluationFinding: Rule _3_4_01_DLIES_KDF verified" // ok
 		);
 	}
 

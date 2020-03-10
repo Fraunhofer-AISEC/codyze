5dd05ebfedf642dc960d7a32ab46843a0cee8a84
Oliver Braunsdorf <oliver.braunsdorf@aisec.fraunhofer.de>
2020-03-10 13:08:21 +0100

    finalize Botan TR rule test

---
+++ src/test/resources/botan_rule_tr_test/7_2_2_1_01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <botan/auto_rng.h>
+#include <botan/rng.h>
+#include <botan/hex.h>
+#include <botan/dh.h>
+
+int main() {
+    const std::vector<uint8_t> data = Botan::hex_decode("6BC1BEE22E409F96E93D7E117393172A");
+    const std::vector<uint8_t> test_sig = Botan::hex_decode("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    Botan::AutoSeeded_RNG rng;
+    Botan::DL_Group dl_group("dsa/botan/3072");
+    Botan::DH_PrivateKey(rng, dl_group);
+}

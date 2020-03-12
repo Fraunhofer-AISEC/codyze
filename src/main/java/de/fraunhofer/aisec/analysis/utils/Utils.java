6ba10a8cbb8e01c7c0f10b8dd4aef2c3e7c35711
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2020-03-12 21:18:46 +0100

    TUI: Help message is automatically generated from methods annotated with @ShellCommand

--- src/main/java/de/fraunhofer/aisec/analysis/utils/Utils.java
+++ src/main/java/de/fraunhofer/aisec/analysis/utils/Utils.java
@@ -14,8 +14,9 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
-import java.util.Set;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Utils {
@@ -192,5 +193,24 @@ public class Utils {
 		log.debug("Any of {} is a subtype of {}: {}", sourceTypes.stream().map(Type::getTypeName).collect(Collectors.joining(",")), markParameter, result);
 
 		return result;
+	}
+
+	public static List<Method> getMethodsAnnotatedWith(final Class<?> type, final Class<? extends Annotation> annotation) {
+		final List<Method> methods = new ArrayList<Method>();
+		Class<?> klass = type;
+		while (klass != Object.class) { // need to iterated thought hierarchy in order to retrieve methods from above the current instance
+			// iterate though the list of methods declared in the class represented by klass variable, and add those annotated with the specified annotation
+			final List<Method> allMethods = new ArrayList<Method>(Arrays.asList(klass.getDeclaredMethods()));
+			for (final Method method : allMethods) {
+				if (method.isAnnotationPresent(annotation)) {
+					Annotation annotInstance = method.getAnnotation(annotation);
+					// TODO process annotInstance
+					methods.add(method);
+				}
+			}
+			// move to the upper class in the hierarchy in search for more methods
+			klass = klass.getSuperclass();
+		}
+		return methods;
 	}
 }

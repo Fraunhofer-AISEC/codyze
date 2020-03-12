5a73f98ae2e2d7f7d57377c6fab60ee8fb8aa6db
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2020-03-12 11:19:08 +0100

    Unified naming of Builtin classes

--- src/main/java/de/fraunhofer/aisec/crymlin/builtin/DirectEogConnection.java
+++ src/main/java/de/fraunhofer/aisec/crymlin/builtin/DirectEogConnection.java
@@ -15,14 +15,15 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 
 /**
- * this Builtin checks if there is an arbitrary EOG-connection between the two given vertices (responsible for the markvars given as parameters)
+ * this Builtin checks if there is a _direct_ EOG-connection (i.e., without branches) between the two given vertices (responsible for the markvars given as parameters)
  */
-public class EogConnectionBuiltin implements Builtin {
-	private static final Logger log = LoggerFactory.getLogger(EogConnectionBuiltin.class);
+
+public class DirectEogConnection implements Builtin {
+	private static final Logger log = LoggerFactory.getLogger(DirectEogConnection.class);
 
 	@Override
 	public @NonNull String getName() {
-		return "_eog_connection";
+		return "_direct_eog_connection";
 	}
 
 	@Override
@@ -36,7 +37,7 @@ public class EogConnectionBuiltin implements Builtin {
 			List<Vertex> vertices = BuiltinHelper.extractResponsibleVertices(argResultList, 2);
 			// now we have one vertex each for arg0 and arg1, both not null
 
-			ConstantValue ret = ConstantValue.of(CrymlinQueryWrapper.eogConnection(vertices.get(0), vertices.get(1), true));
+			ConstantValue ret = ConstantValue.of(CrymlinQueryWrapper.eogConnection(vertices.get(0), vertices.get(1), false));
 			ret.addResponsibleVertices(vertices.get(0), vertices.get(1));
 			return ret;
 

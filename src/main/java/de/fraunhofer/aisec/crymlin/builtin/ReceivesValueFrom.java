715d69eb8c5efd0ce4da6cb0b428762bc61f3321
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2020-03-12 11:03:44 +0100

    Implement _receives_value_from

--- src/main/java/de/fraunhofer/aisec/crymlin/builtin/ReceivesValueFrom.java
+++ src/main/java/de/fraunhofer/aisec/crymlin/builtin/ReceivesValueFrom.java
@@ -6,10 +6,21 @@ import de.fraunhofer.aisec.analysis.structures.ConstantValue;
 import de.fraunhofer.aisec.analysis.structures.ErrorValue;
 import de.fraunhofer.aisec.analysis.structures.ListValue;
 import de.fraunhofer.aisec.analysis.structures.MarkContextHolder;
+import de.fraunhofer.aisec.crymlin.CrymlinQueryWrapper;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.*;
+
+/**
+ * Method signature: _receives_value_from(var target, var source).
+ *
+ * Returns true, if there is a data flow from source to target.
+ *
+ * Data flows are only traced intraprocedurally and this builtin will return true if <i>any</i> flow exists.
+ */
 public class ReceivesValueFrom implements Builtin {
 	private static final Logger log = LoggerFactory.getLogger(ReceivesValueFrom.class);
 
@@ -25,7 +36,35 @@ public class ReceivesValueFrom implements Builtin {
 			MarkContextHolder markContextHolder,
 			ExpressionEvaluator expressionEvaluator) {
 
-		log.error("not Implemented");
-		return ErrorValue.newErrorValue("_receives_value_from not implemented");
+		try {
+			// We expect a source and a target vertex
+			List<Vertex> vertices = BuiltinHelper.extractResponsibleVertices(argResultList, 2);
+			if (vertices.size() != 2) {
+				return ErrorValue.newErrorValue("Could not resolve arguments of _receives_value_from");
+			}
+			Vertex targetV = vertices.get(0);
+			Vertex sourceV = vertices.get(1);
+
+			// Follow DFG edges backwards, avoiding loops
+			Set<Vertex> seen = new HashSet<>();
+			Deque<Vertex> worklist = new ArrayDeque<>();
+			worklist.addAll(CrymlinQueryWrapper.getDFGSources(targetV));
+			while (!worklist.isEmpty()) {
+				Vertex currentV = worklist.pop();
+				if (seen.contains(currentV)) {
+					continue;
+				}
+				seen.add(currentV);
+				if (currentV.id().equals(sourceV.id())) {
+					return ConstantValue.of(true);
+				}
+				worklist.addAll(CrymlinQueryWrapper.getDFGSources(targetV));
+			}
+		}
+		catch (InvalidArgumentException e) {
+			e.printStackTrace();
+			return ErrorValue.newErrorValue(e.getMessage() + " in _receives_value_from");
+		}
+		return ErrorValue.newErrorValue("Error in _receives_value_from");
 	}
 }

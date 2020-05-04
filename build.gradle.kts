c0e3ed2447b1ac6bc8490f27bc2ce7fcf1aad74a
Julian Schuette <julian.schuette@gmx.net>
2020-05-04 23:06:49 +0200

    Switch to cpg-1.4.3-SNAPSHOT, Fix RealBCTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,7 @@ val versions = mapOf(
         "tinkerpop" to "3.4.3",
         "neo4j-gremlin-bolt" to "0.3.1",
         "xml.bind" to "2.3.1",
-        "cpg" to "1.4.0",
+        "cpg" to "1.4.3-SNAPSHOT",
         "json" to "20190722"        
 )
 

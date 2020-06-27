b0af01b731b665a1ea88f5ba449b568701013608
Julian Schuette <julian.schuette@gmx.net>
2020-06-27 08:39:38 +0200

    Description format for findings includes SARIF-like fields "descriptionShort", "descriptionFull". #14

--- src/main/java/de/fraunhofer/aisec/analysis/structures/FindingDescription.java
+++ src/main/java/de/fraunhofer/aisec/analysis/structures/FindingDescription.java
@@ -1,28 +1,37 @@
 
 package de.fraunhofer.aisec.analysis.structures;
 
-import org.json.*;
+import com.jayway.jsonpath.*;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 
+/**
+ * Human-readable description of a finding (i.e. of an analysis result).
+ */
 public class FindingDescription {
 	private static final Logger log = LoggerFactory.getLogger(FindingDescription.class);
 
-	private JSONObject source = null;
 	private static FindingDescription instance = null;
+	private DocumentContext json;
 
 	private FindingDescription() {
 		/* do not instantiate */
 	}
 
+	/**
+	 * Singleton.
+	 *
+	 * Currently, there can be only one set of descriptions. This may change in the future when multiple MARK sources are supported.
+	 *
+	 * @return
+	 */
 	public static FindingDescription getInstance() {
 		if (instance == null) {
 			instance = new FindingDescription();
@@ -30,51 +39,56 @@ public class FindingDescription {
 		return instance;
 	}
 
-	public String getDescriptionDetailed(String constant) {
-		if (source == null) {
-			return constant;
+	@Nullable
+	public String getDescriptionFull(@NonNull final String onFailId) {
+		try {
+			return json.read("$['" + onFailId + "']['fullDescription']['text']");
 		}
-
-		if (source.has(constant)) {
-			JSONObject jsonObject = source.getJSONObject(constant);
-			if (jsonObject.has("detailed")) {
-				return jsonObject.getString("detailed");
-			}
+		catch (PathNotFoundException e) {
+			return null;
 		}
-
-		return constant;
 	}
 
-	public String getDescriptionBrief(String constant) {
-		if (source == null) {
-			return constant;
+	@Nullable
+	public String getDescriptionShort(@NonNull final String onFailId) {
+		try {
+			return json.read("$['" + onFailId + "']['shortDescription']['text']");
 		}
-
-		if (source.has(constant)) {
-			JSONObject jsonObject = source.getJSONObject(constant);
-			if (jsonObject.has("brief")) {
-				return jsonObject.getString("brief");
-			}
+		catch (PathNotFoundException e) {
+			return null;
 		}
-
-		return constant;
 	}
 
-	public void init(File descriptionFile) {
+	@Nullable
+	public String getHelpUri(@NonNull final String onFailId) {
+		try {
+			return json.read("$['" + onFailId + "']['helpUri']");
+		}
+		catch (PathNotFoundException e) {
+			return null;
+		}
+	}
+
+	@Nullable
+	public List<String> getFixes(@NonNull final String onFailId) {
+		try {
+			return json.read("$['" + onFailId + "']['fixes'][*]['description']['text']");
+		}
+		catch (PathNotFoundException e) {
+			return null;
+		}
+	}
+
+	public void init(@NonNull File descriptionFile) {
 		log.info("Parsing MARK description file from {}", descriptionFile.getAbsolutePath());
-		try (
-				InputStream inputStream = new FileInputStream(descriptionFile);
-				BufferedReader in = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));) {
+		try {
+			Configuration conf = Configuration.defaultConfiguration();
+			conf.addOptions(Option.DEFAULT_PATH_LEAF_TO_NULL);
+			conf.addOptions(Option.SUPPRESS_EXCEPTIONS); // Return null instead of exception
 
-			StringBuilder sb = new StringBuilder();
-			String readLine;
+			json = JsonPath.using(conf).parse(descriptionFile);
 
-			while ((readLine = in.readLine()) != null) {
-				sb.append(readLine);
-				sb.append("\n");
-			}
-			source = new JSONObject(sb.toString());
-			log.info("Loaded {} descriptions", source.length());
+			log.info("Loaded {} descriptions", ((Map) json.read("$")).size());
 		}
 		catch (IOException e) {
 			log.warn("Failed to load findingDescription.json, explanations will be empty", e);

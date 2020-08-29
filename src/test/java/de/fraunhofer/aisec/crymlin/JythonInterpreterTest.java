185f86f9c7f6e9ef27e0d58499288b19db8a126c
Banse, Christian <christian.banse@aisec.fraunhofer.de>
2020-08-29 22:25:17 +0200

    Fixed tests

--- src/test/java/de/fraunhofer/aisec/crymlin/JythonInterpreterTest.java
+++ src/test/java/de/fraunhofer/aisec/crymlin/JythonInterpreterTest.java
@@ -2,12 +2,24 @@
 package de.fraunhofer.aisec.crymlin;
 
 import de.fraunhofer.aisec.analysis.JythonInterpreter;
+import de.fraunhofer.aisec.analysis.structures.ServerConfiguration;
+import de.fraunhofer.aisec.crymlin.connectors.db.OverflowDatabase;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.python.core.Py;
 import org.python.util.JLineConsole;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -15,221 +27,230 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
-import static de.fraunhofer.aisec.analysis.JythonInterpreter.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static de.fraunhofer.aisec.analysis.JythonInterpreter.PY_G;
+import static de.fraunhofer.aisec.analysis.JythonInterpreter.PY_GRAPH;
+import static de.fraunhofer.aisec.analysis.JythonInterpreter.PY_Q;
+import static de.fraunhofer.aisec.analysis.JythonInterpreter.PY_QUERY;
+import static de.fraunhofer.aisec.analysis.JythonInterpreter.PY_S;
+import static de.fraunhofer.aisec.analysis.JythonInterpreter.PY_SERVER;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/** Testing the Crymlin console. */
+/**
+ * Testing the Crymlin console.
+ */
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class JythonInterpreterTest {
 
-	private static JLineConsole jlineConsole;
-	private static PrintStream originalErr;
-	private static PrintStream originalOut;
-	private static ByteArrayOutputStream outContent;
-	private static ByteArrayOutputStream errContent;
-	private static InputStream oldIn;
-	private static JythonInterpreter interp;
+    private static JLineConsole jlineConsole;
+    private static PrintStream originalErr;
+    private static PrintStream originalOut;
+    private static ByteArrayOutputStream outContent;
+    private static ByteArrayOutputStream errContent;
+    private static InputStream oldIn;
+    private static JythonInterpreter interp;
 
-	/**
-	 * The console reads and prints from/to stdin/stdout.
-	 * We redirect these streams to access them from these tests.
-	 *
-	 * @throws IOException
-	 * @throws InterruptedException
-	 */
-	@BeforeAll
-	public static void beforeAll() throws IOException, InterruptedException {
-		// Connect a POS to stdin
-		PipedOutputStream pos = new PipedOutputStream();
-		PipedInputStream pis = new PipedInputStream(pos);
-		oldIn = System.in;
-		System.setIn(pis);
+    /**
+     * The console reads and prints from/to stdin/stdout.
+     * We redirect these streams to access them from these tests.
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @BeforeAll
+    public static void beforeAll() throws IOException, InterruptedException {
+        // Connect a POS to stdin
+        PipedOutputStream pos = new PipedOutputStream();
+        PipedInputStream pis = new PipedInputStream(pos);
+        oldIn = System.in;
+        System.setIn(pis);
 
-		// Redirect stdout, stderr to BOS
-		outContent = new ByteArrayOutputStream();
-		errContent = new ByteArrayOutputStream();
-		originalOut = System.out;
-		originalErr = System.err;
-		System.setOut(new PrintStream(outContent));
-		System.setErr(new PrintStream(errContent));
+        // Redirect stdout, stderr to BOS
+        outContent = new ByteArrayOutputStream();
+        errContent = new ByteArrayOutputStream();
+        originalOut = System.out;
+        originalErr = System.err;
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
 
-		// Trick to make InputStreamReader for stdin blocking to avoid exception when reading from pis.
-		System.setProperty("org.python.jline.esc.timeout", "0");
+        // Trick to make InputStreamReader for stdin blocking to avoid exception when reading from pis.
+        System.setProperty("org.python.jline.esc.timeout", "0");
 
-		// Start console (in thread, because it will block)
-		interp = new JythonInterpreter();
-		new Thread(() -> {
-			interp.spawnInteractiveConsole();
-		}).start();
+        // Start console (in thread, because it will block)
+        interp = new JythonInterpreter();
+        new Thread(() -> interp.spawnInteractiveConsole()).start();
 
-		// Wait until console is up
-		ReentrantLock lock = new ReentrantLock();
-		Condition consoleAvailable = lock.newCondition();
-		new Thread(() -> {
-			lock.lock();
-			while (interp.getConsole() == null) {
-				try {
-					//noinspection BusyWait
-					Thread.sleep(50);
-				}
-				catch (InterruptedException e) {
-					Thread.currentThread()
-							.interrupt();
-				}
-			}
-			consoleAvailable.signalAll();
-			lock.unlock();
-		}).start();
-		lock.lock();
-		consoleAvailable.await(100, TimeUnit.SECONDS);
-		lock.unlock();
+        // Wait until console is up
+        ReentrantLock lock = new ReentrantLock();
+        Condition consoleAvailable = lock.newCondition();
+        new Thread(() -> {
+            lock.lock();
+            while (interp.getConsole() == null) {
+                try {
+                    //noinspection BusyWait
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    Thread.currentThread()
+                            .interrupt();
+                }
+            }
+            // instead of running a full analysis, just provide access to an empty database
+            interp.connect(new OverflowDatabase(ServerConfiguration.builder().disableOverflow(true).build()));
 
-		jlineConsole = (JLineConsole) Py.getConsole();
+            consoleAvailable.signalAll();
+            lock.unlock();
+        }).start();
+        lock.lock();
+        consoleAvailable.await(100, TimeUnit.SECONDS);
+        lock.unlock();
 
-		assertNotNull(jlineConsole);
-		assertNotNull(outContent);
-		assertNotNull(errContent);
-	}
+        jlineConsole = (JLineConsole) Py.getConsole();
 
-	/**
-	 * Test behavior of tab completion if no input exists.
-	 *
-	 * @throws Exception
-	 */
-	@Test
-	@Order(1)
-	public void completionSimpleTest() throws Exception {
-		List<CharSequence> completions = new ArrayList<>();
-		jlineConsole.getReader()
-				.getCompleters()
-				.iterator()
-				.next()
-				.complete("\t", 0, completions);
-		outContent.flush();
-		errContent.flush();
+        assertNotNull(jlineConsole);
+        assertNotNull(outContent);
+        assertNotNull(errContent);
+    }
 
-		assertTrue(completions.contains(PY_QUERY));
-		assertTrue(completions.contains(PY_Q));
-		assertTrue(completions.contains(PY_SERVER));
-		assertTrue(completions.contains(PY_S));
-		assertTrue(completions.contains(PY_GRAPH));
-		assertTrue(completions.contains(PY_G));
-	}
+    /**
+     * Test behavior of tab completion if no input exists.
+     *
+     * @throws Exception
+     */
+    @Test
+    @Order(1)
+    public void completionSimpleTest() throws Exception {
+        List<CharSequence> completions = new ArrayList<>();
+        jlineConsole.getReader()
+                .getCompleters()
+                .iterator()
+                .next()
+                .complete("\t", 0, completions);
+        outContent.flush();
+        errContent.flush();
 
-	/**
-	 * Test behavior of tab completion for "server.<TAB>"
-	 *
-	 * @throws Exception
-	 */
-	@Test
-	@Order(2)
-	public void completionServerObjectTest() throws Exception {
-		List<CharSequence> completions = new ArrayList<>();
-		jlineConsole.getReader()
-				.getCompleters()
-				.iterator()
-				.next()
-				.complete("server.\t", 0, completions);
-		outContent.flush();
-		errContent.flush();
+        assertTrue(completions.contains(PY_QUERY));
+        assertTrue(completions.contains(PY_Q));
+        assertTrue(completions.contains(PY_SERVER));
+        assertTrue(completions.contains(PY_S));
+        assertTrue(completions.contains(PY_GRAPH));
+        assertTrue(completions.contains(PY_G));
+    }
 
-		assertTrue(completions.contains("help()"));
-		assertTrue(completions.contains("show_findings()"));
-		assertTrue(completions.contains("load_rules()"));
-		assertTrue(completions.contains("list_rules()"));
-		assertTrue(completions.contains("analyze()"));
-	}
+    /**
+     * Test behavior of tab completion for "server.<TAB>"
+     *
+     * @throws Exception
+     */
+    @Test
+    @Order(2)
+    public void completionServerObjectTest() throws Exception {
+        List<CharSequence> completions = new ArrayList<>();
+        jlineConsole.getReader()
+                .getCompleters()
+                .iterator()
+                .next()
+                .complete("server.\t", 0, completions);
+        outContent.flush();
+        errContent.flush();
 
-	/**
-	 * Test behavior of tab completion for "server.sho<TAB>"
-	 *
-	 * @throws Exception
-	 */
-	@Test
-	@Order(3)
-	public void completionServerObjectTest2() throws Exception {
-		List<CharSequence> completions = new ArrayList<>();
-		jlineConsole.getReader()
-				.getCompleters()
-				.iterator()
-				.next()
-				.complete("server.sho\t", 0, completions);
-		outContent.flush();
-		errContent.flush();
+        assertTrue(completions.contains("help()"));
+        assertTrue(completions.contains("show_findings()"));
+        assertTrue(completions.contains("load_rules()"));
+        assertTrue(completions.contains("list_rules()"));
+        assertTrue(completions.contains("analyze()"));
+    }
 
-		assertTrue(completions.contains("show_findings()"));
-		assertEquals(1, completions.size());
-	}
+    /**
+     * Test behavior of tab completion for "server.sho<TAB>"
+     *
+     * @throws Exception
+     */
+    @Test
+    @Order(3)
+    public void completionServerObjectTest2() throws Exception {
+        List<CharSequence> completions = new ArrayList<>();
+        jlineConsole.getReader()
+                .getCompleters()
+                .iterator()
+                .next()
+                .complete("server.sho\t", 0, completions);
+        outContent.flush();
+        errContent.flush();
 
-	/**
-	 * Test behavior of tab completion for "server.sho<TAB>"
-	 *
-	 * @throws Exception
-	 */
-	@Test
-	@Order(4)
-	public void completionQueryObjectTest() throws Exception {
-		List<CharSequence> completions = new ArrayList<>();
-		jlineConsole.getReader()
-				.getCompleters()
-				.iterator()
-				.next()
-				.complete("query.cal\t", 0, completions);
-		outContent.flush();
-		errContent.flush();
+        assertTrue(completions.contains("show_findings()"));
+        assertEquals(1, completions.size());
+    }
 
-		assertTrue(completions.contains("calls()"));
-		assertEquals(2, completions.size());
-	}
+    /**
+     * Test behavior of tab completion for "server.sho<TAB>"
+     *
+     * @throws Exception
+     */
+    @Test
+    @Order(4)
+    public void completionQueryObjectTest() throws Exception {
+        List<CharSequence> completions = new ArrayList<>();
+        jlineConsole.getReader()
+                .getCompleters()
+                .iterator()
+                .next()
+                .complete("query.cal\t", 0, completions);
+        outContent.flush();
+        errContent.flush();
 
-	/**
-	 * Test behavior of tab completion for "query.allCalls().<TAB>"
-	 *
-	 * @throws Exception
-	 */
-	@Test
-	@Order(5)
-	public void completionQueryObjectTest2() throws Exception {
-		List<CharSequence> completions = new ArrayList<>();
-		jlineConsole.getReader()
-				.getCompleters()
-				.iterator()
-				.next()
-				.complete("q.calls().\t", 0, completions);
-		outContent.flush();
-		errContent.flush();
+        assertTrue(completions.contains("calls()"));
+        assertEquals(2, completions.size());
+    }
 
-		assertTrue(completions.contains("next()"));
-	}
+    /**
+     * Test behavior of tab completion for "query.allCalls().<TAB>"
+     *
+     * @throws Exception
+     */
+    @Test
+    @Order(5)
+    public void completionQueryObjectTest2() throws Exception {
+        List<CharSequence> completions = new ArrayList<>();
+        jlineConsole.getReader()
+                .getCompleters()
+                .iterator()
+                .next()
+                .complete("q.calls().\t", 0, completions);
+        outContent.flush();
+        errContent.flush();
 
-	@Test
-	@Order(6)
-	public void simpleJythonTest() throws Exception {
-		// Just for testing: We can run normal Gremlin queries:
-		Object result = interp.query("q.V([]).toSet()"); // Get all (!) nodes
-		assertEquals(HashSet.class, result.getClass());
-	}
+        assertTrue(completions.contains("next()"));
+    }
 
-	@Test
-	@Order(7)
-	public void crymlinOverJythonTest() throws Exception {
-		// Run crymlin queries as strings and get back the results as Java objects:
-		List<Vertex> classes = (List<Vertex>) interp.query("crymlin.records().toList()");
-		assertNotNull(classes);
+    @Test
+    @Order(6)
+    public void simpleJythonTest() throws Exception {
+        // Just for testing: We can run normal Gremlin queries:
+        Object result = interp.query("q.V([]).toSet()"); // Get all (!) nodes
+        assertEquals(HashSet.class, result.getClass());
+    }
 
-		List<Vertex> literals = (List<Vertex>) interp.query("crymlin.sourcefiles().literals().toList()");
-		assertNotNull(literals);
-	}
+    @Test
+    @Order(7)
+    public void crymlinOverJythonTest() throws Exception {
+        // Run crymlin queries as strings and get back the results as Java objects:
+        List<Vertex> classes = (List<Vertex>) interp.query("crymlin.records().toList()");
+        assertNotNull(classes);
 
-	@AfterAll
-	public static void afterAll() {
-		// Restore system streams
-		System.setOut(originalOut);
-		System.setErr(originalErr);
-		System.setIn(oldIn);
+        List<Vertex> literals = (List<Vertex>) interp.query("crymlin.sourcefiles().literals().toList()");
+        assertNotNull(literals);
+    }
 
-		interp.close();
-		jlineConsole.getReader().close();
-	}
+    @AfterAll
+    public static void afterAll() {
+        // Restore system streams
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+        System.setIn(oldIn);
+
+        interp.close();
+        jlineConsole.getReader().close();
+    }
 
 }

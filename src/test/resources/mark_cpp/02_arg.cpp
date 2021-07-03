5a06e106f6810d70194ae0ee6fca30cc24414b0a
Christian Banse <christian.banse@aisec.fraunhofer.de>
2021-07-03 19:28:26 +0200

    Fixed syntax error in test file that caused confusion. However this is related to https://github.com/Fraunhofer-AISEC/cpg/issues/455

--- src/test/resources/mark_cpp/02_arg.cpp
+++ src/test/resources/mark_cpp/02_arg.cpp
@@ -1,14 +1,14 @@
 class Test {
 
 public:
-int call(int a) {
-  return a + 1;
-}
-}
+  int call(int a) {
+    return a + 1;
+  }
+};
 
 int main() {
   int foo = 42;
   foo = 3;
-  Test t();
+  Test t;
   t.call(foo);
 }

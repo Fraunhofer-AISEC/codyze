644387c62f95c16116fce9c50656818483f93f25
fwendland <fwendland@users.noreply.github.com>
2021-07-08 13:25:35 +0200

    Adds builtin `_now()` and `_year()` (#229)
    Builtin `_now()` returns the current time at execution as seconds
    since epoch -- 1970-01-01T00:00:00Z.
    
    Builtin `_year()` takes an integer argument, interprets it as seconds
    since epoch and returns the current year.
    
    `_now()` and `_year()` can be used in conjunction to create time
    dependent rules. For instance, one can write rules that are valid
    for a predetermined number of years. Afterwards, these rules are
    considered obsolete.
    
    To implement zero parameter builtins, `Builtin` was retrofitted with
    a flag to indicate whether it has parameters.

---
+++ src/main/java/de/fraunhofer/aisec/crymlin/builtin/Year.java
@@ -0,0 +1,76 @@
+
+package de.fraunhofer.aisec.crymlin.builtin;
+
+import de.fraunhofer.aisec.analysis.markevaluation.ExpressionEvaluator;
+import de.fraunhofer.aisec.analysis.markevaluation.ExpressionHelper;
+import de.fraunhofer.aisec.analysis.structures.*;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalField;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ * Method signature: _year()
+ *
+ * Note:
+ * Time is represented as numeric value. The value represents the number of seconds
+ * since epoch (1970-01-01T00:00:00Z).
+ */
+public class Year implements Builtin {
+	private static final Logger log = LoggerFactory.getLogger(Year.class);
+
+	@NonNull
+	@Override
+	public String getName() {
+		return "_year";
+	}
+
+	@NonNull
+	@Override
+	public ConstantValue execute(
+			@NonNull AnalysisContext ctx,
+			@NonNull ListValue argResultList,
+			@NonNull Integer contextID,
+			@NonNull MarkContextHolder markContextHolder,
+			@NonNull ExpressionEvaluator expressionEvaluator) {
+
+		// arguments: int
+		// example:
+		// _year(_now()) returns 2021
+
+		try {
+			BuiltinHelper.verifyArgumentTypesOrThrow(argResultList, ConstantValue.class);
+
+			var epochSeconds = ExpressionHelper.asNumber(argResultList.get(0));
+
+			if (epochSeconds == null) {
+				log.warn("The argument for _year was not the expected type, or not initialized/resolved");
+				return ErrorValue.newErrorValue("The argument for _year was not the expected type, or not initialized/resolved", argResultList.getAll());
+			}
+
+			log.info("arg: {}", epochSeconds);
+
+			var instant = Instant.ofEpochSecond((Long) epochSeconds);
+			var ldt = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+			var year = ldt.getYear();
+
+			log.debug("_year() -> {}", year);
+
+			ConstantValue cv = ConstantValue.of(year);
+			cv.addResponsibleVerticesFrom((ConstantValue) argResultList.get(0));
+
+			return cv;
+		}
+		catch (InvalidArgumentException e) {
+			log.warn(e.getMessage());
+			return ErrorValue.newErrorValue(e.getMessage(), argResultList.getAll());
+		}
+	}
+}
---
+++ src/test/resources/builtins/year.c
@@ -0,0 +1,3 @@
+int main() {
+    test("a");
+}

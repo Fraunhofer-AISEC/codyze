93a13946988d68b2ec438e609f2054f21a221e9a
Maximilian Kaul <maximilian.kaul@aisec.fraunhofer.de>
2021-08-27 17:21:29 +0200

    rename test

--- src/test/java/de/fraunhofer/aisec/codyze/crymlin/OrderTestCStyle.kt
+++ src/test/java/de/fraunhofer/aisec/codyze/crymlin/OrderTestCStyle.kt
@@ -5,7 +5,7 @@ import kotlin.test.assertTrue
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
-class OrderIssueTest : AbstractMarkTest() {
+class OrderTestCStyle : AbstractMarkTest() {
 
     // this test case fails because currently, the WPDS cannot handle the direct assignment of the
     // variable to the call of the rule
@@ -13,7 +13,7 @@ class OrderIssueTest : AbstractMarkTest() {
     @Test
     fun checkOrderWPDS_foo1() {
         this.tsMode = TypestateMode.WPDS
-        val results = performTest("unittests/order_issue_foo1.c", "unittests/order_issue.mark")
+        val results = performTest("unittests/order_foo1.c", "unittests/order_cstyle.mark")
 
         // only good findings
         assertTrue(results.all { !it.isProblem })
@@ -22,7 +22,7 @@ class OrderIssueTest : AbstractMarkTest() {
     @Test
     fun checkOrderWPDS_foo2() {
         this.tsMode = TypestateMode.WPDS
-        val results = performTest("unittests/order_issue_foo2.c", "unittests/order_issue.mark")
+        val results = performTest("unittests/order_foo2.c", "unittests/order_cstyle.mark")
 
         // only good findings
         assertTrue(results.all { !it.isProblem })
@@ -34,7 +34,7 @@ class OrderIssueTest : AbstractMarkTest() {
     fun checkOrderWPDS_both() {
         // fail
         this.tsMode = TypestateMode.WPDS
-        val results = performTest("unittests/order_issue.c", "unittests/order_issue.mark")
+        val results = performTest("unittests/order.c", "unittests/order_cstyle.mark")
 
         // only good findings
         assertTrue(results.all { !it.isProblem })
@@ -43,7 +43,7 @@ class OrderIssueTest : AbstractMarkTest() {
     @Test
     fun checkOrderNFA_foo1() {
         this.tsMode = TypestateMode.NFA
-        val results = performTest("unittests/order_issue_foo1.c", "unittests/order_issue.mark")
+        val results = performTest("unittests/order_foo1.c", "unittests/order_cstyle.mark")
 
         // only good findings
         assertTrue(results.all { !it.isProblem })
@@ -51,7 +51,7 @@ class OrderIssueTest : AbstractMarkTest() {
     @Test
     fun checkOrderNFA_foo2() {
         this.tsMode = TypestateMode.NFA
-        val results = performTest("unittests/order_issue_foo2.c", "unittests/order_issue.mark")
+        val results = performTest("unittests/order_foo2.c", "unittests/order_cstyle.mark")
 
         // only good findings
         assertTrue(results.all { !it.isProblem })
@@ -59,7 +59,7 @@ class OrderIssueTest : AbstractMarkTest() {
     @Test
     fun checkOrderNFA_both() {
         this.tsMode = TypestateMode.NFA
-        val results = performTest("unittests/order_issue.c", "unittests/order_issue.mark")
+        val results = performTest("unittests/order.c", "unittests/order_cstyle.mark")
 
         // only good findings
         assertTrue(results.all { !it.isProblem })

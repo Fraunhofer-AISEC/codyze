ed0dca1abd2b8909b20ee55fad3974c642609d4a
Wendland, Florian <florian.wendland@aisec.fraunhofer.de>
2021-09-10 16:28:12 +0200

    Change evaluation of `when` to fail on errors and fix rule mistakes
    The `when` prevents the evaluation of the `ensure` in a rule. It acts as a
    guard. Errors in the evaluation of the `when` clause used to be ignored. This
    caused an evaluation of the `ensure` clause.
    
    This changes the behaviour to fail and ignore the `ensure` clause. Thus,
    errors in `when` are now considered to mean `false`. This prevents the
    unnecessary evaluation of rules for example when a corresponding node
    wasn't found.
    
    As a side effect, mistakes in rules have been identified and fixed as well.

--- src/main/java/de/fraunhofer/aisec/codyze/analysis/markevaluation/Evaluator.java
+++ src/main/java/de/fraunhofer/aisec/codyze/analysis/markevaluation/Evaluator.java
@@ -307,10 +307,14 @@ public class Evaluator {
 					log.warn("Unable to evaluate when-part of rule {}, result had an error: \n\t{}", rule.getName(),
 						((ErrorValue) entry.getValue()).getDescription().replace("\n", "\n\t"));
 					// FIXME do we want to evaluate the ensure-part if the when-part had an error?
-					continue;
+					//continue;
+					// assume error in `when` means false
+					markCtxHolder.removeContext(entry.getKey());
 				} else if (!(value instanceof Boolean)) {
 					log.error("Unable to evaluate when-part of rule {}, result is not a boolean, but {}", rule.getName(), value.getClass().getSimpleName());
-					continue;
+					//continue;
+					// assume error in `when` means false
+					markCtxHolder.removeContext(entry.getKey());
 				}
 
 				if (value.equals(false) || ConstantValue.isError(entry.getValue())) {

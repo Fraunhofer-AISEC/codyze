5f19895ed0b8bf4cee5eb02e6db0a82ac2f21e2f
Christian Banse <christian.banse@aisec.fraunhofer.de>
2021-09-19 10:18:25 +0200

    Cleanup

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,12 +1,11 @@
 /* --------------------------------------------------------------------------------------------
- * Based on https://github.com/microsoft/vscode-extension-samples, Copyright (c) Microsoft Corporation under MIT License
+ * Based on https://github.com/microsoft/vscode-extension-samples under MIT License
  *
  * Adapted for Codyze. License: Apache 2.0
  * ------------------------------------------------------------------------------------------ */
 
 import * as path from 'path';
 import { ExtensionContext, workspace } from 'vscode';
-
 import {
 	LanguageClient,
 	LanguageClientOptions,
@@ -16,23 +15,29 @@ import {
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
+	// We need to figure out the path to our MARK files
 	let markPath = context.asAbsolutePath(path.join("codyze", "mark"))
 
-	let serverModule = context.asAbsolutePath(
+	// This is the path to our server binary
+	let serverBinaryPath = context.asAbsolutePath(
 		path.join('codyze', 'bin', 'codyze')
 	);
 
-	// If the extension is launched in debug mode then the debug server options are used
+	// If the extension is launched in debug mode then the debug server options are used.
 	// Otherwise the run options are used
 	let serverOptions: ServerOptions = {
-		run: { command: serverModule, args: ["-l", "-m", markPath] },
-		debug: { command: serverModule, args: ["-l", "-m", markPath] }
+		run: { command: serverBinaryPath, args: ["-l", "-m", markPath] },
+		debug: { command: serverBinaryPath, args: ["-l", "-m", markPath] }
 	};
 
 	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'cpp' }, { scheme: 'file', language: 'java' }],
+		// Register the server for C/C++ and Java files
+		documentSelector: [
+			{ scheme: 'file', language: 'c' },
+			{ scheme: 'file', language: 'cpp' },
+			{ scheme: 'file', language: 'java' }
+		],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
@@ -47,7 +52,7 @@ export function activate(context: ExtensionContext) {
 		clientOptions
 	);
 
-	// Start the client. This will also launch the server
+	// Start the client. This will also launch the server binary
 	client.start();
 }
 

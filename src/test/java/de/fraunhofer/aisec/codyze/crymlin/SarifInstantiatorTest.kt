bfb195937b5b351b2bf072a087b95a1f2b592910
Robert Haimerl <robert.haimerl@aisec.fraunhofer.de>
2021-11-18 08:17:53 +0100

    created a Finding to use in the Test

--- src/test/java/de/fraunhofer/aisec/codyze/crymlin/SarifInstantiatorTest.kt
+++ src/test/java/de/fraunhofer/aisec/codyze/crymlin/SarifInstantiatorTest.kt
@@ -1,6 +1,13 @@
 package de.fraunhofer.aisec.codyze.crymlin
 
+import de.fraunhofer.aisec.codyze.analysis.Finding
+import de.fraunhofer.aisec.codyze.analysis.FindingDescription
 import de.fraunhofer.aisec.codyze.analysis.SarifInstantiator
+import de.fraunhofer.aisec.cpg.sarif.Region
+import de.fraunhofer.aisec.mark.markDsl.Action
+import java.io.File
+import java.net.URI
+import kotlin.test.assertNotNull
 import org.junit.jupiter.api.Test
 
 internal class SarifInstantiatorTest {
@@ -8,6 +15,25 @@ internal class SarifInstantiatorTest {
     fun testOutput() {
         val s = SarifInstantiator()
 
-        println(s.getSarif())
+        // "borrowed" the FindingDescriptionTest for a Finding to use
+        // Create some Finding object
+        val logMsg = "Variable cm not initialized"
+        val artifactUri = URI.create("file:///tmp/test.cpp")
+        val id = "WrongUseOfBotan_CipherMode"
+        val regions = listOf(Region(0, 2, 10, 12))
+        val isProblem = true
+        val f = Finding(id, Action.FAIL, logMsg, artifactUri, regions, isProblem)
+
+        // Initialize database with explanations
+        val fd = FindingDescription.instance
+        val url = this.javaClass.classLoader.getResource("unittests/findingDescription.json")
+
+        assertNotNull(url)
+
+        fd.init(File(url.toURI()))
+
+        s.pushRun(setOf(f))
+
+        println(s.toString())
     }
 }

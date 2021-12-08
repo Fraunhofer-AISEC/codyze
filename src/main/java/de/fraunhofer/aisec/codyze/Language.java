cf48286adbd45fb4f6a4d93d6c8c1bc7c811e6d1
Selina Lin <selina.ru-yin.lin@aisec.fraunhofer.de>
2021-12-08 12:43:38 +0100

    remove fields so it compiles

--- src/main/java/de/fraunhofer/aisec/codyze/Language.java
+++ src/main/java/de/fraunhofer/aisec/codyze/Language.java
@@ -1,21 +1,7 @@
 
 package de.fraunhofer.aisec.codyze;
 
-import de.fraunhofer.aisec.cpg.frontends.LanguageFrontend;
-import de.fraunhofer.aisec.cpg.frontends.golang.GoLanguageFrontend;
-import de.fraunhofer.aisec.cpg.frontends.python.PythonLanguageFrontend;
-
-import java.util.List;
-
 public enum Language {
-	PYTHON (PythonLanguageFrontend.class, PythonLanguageFrontend.PY_EXTENSIONS),
-	GO(GoLanguageFrontend.class, GoLanguageFrontend.GOLANG_EXTENSIONS);
-
-	private final Class<? extends LanguageFrontend> languageFrontend;
-	private final List<String> fileTypes;
-
-	Language(Class<? extends LanguageFrontend> languageFrontend, List<String> fileTypes) {
-		this.languageFrontend = languageFrontend;
-		this.fileTypes = fileTypes;
-	}
+	PYTHON,
+	GO
 }

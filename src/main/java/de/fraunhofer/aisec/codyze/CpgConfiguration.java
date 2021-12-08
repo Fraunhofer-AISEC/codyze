5155aacf6e2730e5ae47d8499df33850d9ac7810
Selina Lin <selina.ru-yin.lin@aisec.fraunhofer.de>
2021-12-08 12:41:17 +0100

    add rest of cpg config options

--- src/main/java/de/fraunhofer/aisec/codyze/CpgConfiguration.java
+++ src/main/java/de/fraunhofer/aisec/codyze/CpgConfiguration.java
@@ -3,49 +3,49 @@ package de.fraunhofer.aisec.codyze;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import de.fraunhofer.aisec.cpg.TranslationConfiguration;
 import de.fraunhofer.aisec.cpg.passes.Pass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 
 import java.io.File;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Callable;
 
 public class CpgConfiguration implements Callable<Integer> {
 
 	private boolean debugParser;
-	private List<String> includeWhitelist;
-	private List<String> includeBlacklist;
-	private boolean defaultPasses;
-	private List<Pass> passes;
-	private Map<String, String> symbols;
 	private boolean analyzeIncludes;
 	private File[] includePaths;
+	private List<String> includeWhitelist;
+	private List<String> includeBlacklist;
+	private boolean disableCleanup;
+	private boolean codeInNodes;
+	private boolean processAnnotations;
+	private boolean failOnError;
+	private Map<String, String> symbols;
+	private boolean useUnityBuild;
+	private boolean useParallelFrontends;
+	private boolean typeSystemActiveInFrontend;
+	private boolean defaultPasses;
+	private List<Pass> passes;
+
 	@JsonDeserialize(using = LanguageDeseralizer.class)
 	private EnumSet<Language> additionalLanguages;
+
 	@JsonIgnore
 	@Option(names = {
 			"--enable-python-support" }, description = "Enables the experimental Python support. Additional files need to be placed in certain locations. Please follow the CPG README.")
 	private boolean enablePython;
+
 	@JsonIgnore
 	@Option(names = {
 			"--enable-go-support" }, description = "Enables the experimental Go support. Additional files need to be placed in certain locations. Please follow the CPG README.")
 	private boolean enableGo;
-	@JsonIgnore
+
 	@CommandLine.ArgGroup(exclusive = false, heading = "Translation settings\n")
 	private TranslationSettings translationSettings = new TranslationSettings();
-
-	public TranslationConfiguration buildTranslationSettings() {
-		TranslationConfiguration.Builder builder = new TranslationConfiguration.Builder()
-				.debugParser(debugParser)
-				.codeInNodes(false)
-				.loadIncludes(analyzeIncludes);
-
-		return null;
-	}
 
 	public boolean isAnalyzeIncludes() {
 		return analyzeIncludes;
@@ -125,6 +125,34 @@ public class CpgConfiguration implements Callable<Integer> {
 
 	public void setSymbols(Map<String, String> symbols) {
 		this.symbols = symbols;
+	}
+
+	public void setDisableCleanup(boolean disableCleanup) {
+		this.disableCleanup = disableCleanup;
+	}
+
+	public void setCodeInNodes(boolean codeInNodes) {
+		this.codeInNodes = codeInNodes;
+	}
+
+	public void setProcessAnnotations(boolean processAnnotations) {
+		this.processAnnotations = processAnnotations;
+	}
+
+	public void setFailOnError(boolean failOnError) {
+		this.failOnError = failOnError;
+	}
+
+	public void setUseUnityBuild(boolean useUnityBuild) {
+		this.useUnityBuild = useUnityBuild;
+	}
+
+	public void setUseParallelFrontends(boolean useParallelFrontends) {
+		this.useParallelFrontends = useParallelFrontends;
+	}
+
+	public void setTypeSystemActiveInFrontend(boolean typeSystemActiveInFrontend) {
+		this.typeSystemActiveInFrontend = typeSystemActiveInFrontend;
 	}
 
 	@Override

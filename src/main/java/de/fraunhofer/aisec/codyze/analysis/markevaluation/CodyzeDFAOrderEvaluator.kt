deec7ae7c5a0d3e84c87eee933747b5c51b2fead
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2022-01-14 07:50:31 +0100

    Add Javadoc

--- src/main/java/de/fraunhofer/aisec/codyze/analysis/markevaluation/CodyzeDFAOrderEvaluator.kt
+++ src/main/java/de/fraunhofer/aisec/codyze/analysis/markevaluation/CodyzeDFAOrderEvaluator.kt
@@ -11,6 +11,10 @@ import de.fraunhofer.aisec.cpg.analysis.fsm.DFAOrderEvaluator
 import de.fraunhofer.aisec.cpg.graph.Node
 import java.io.File
 
+/**
+ * Codyze-specific implementation of the [DFAOrderEvaluator].
+ * Its main purpose is to collect the findings in case of violations to the order.
+ */
 class CodyzeDFAOrderEvaluator(
     referencedVertices: Set<Long>,
     nodesToOp: Map<Node, String>,
@@ -20,6 +24,10 @@ class CodyzeDFAOrderEvaluator(
     val ctx: AnalysisContext
 ) : DFAOrderEvaluator(referencedVertices, nodesToOp, thisPositionOfNode) {
 
+    /**
+     * Collects the finding in the AnalysisContext as the [node] makes an
+     * operation which violates the desired order.
+     */
     override fun actionMissingTransitionForNode(node: Node, fsm: DFA) {
         val region = Utils.getRegionByNode(node)
         val outgoing = mutableListOf<String>()
@@ -55,6 +63,11 @@ class CodyzeDFAOrderEvaluator(
         log.info("Finding: {}", f)
     }
 
+    /**
+     * Collects the finding in the AnalysisContext because the DFA finished
+     * analyzing the function but the [base] did not terminate in an accepting
+     * state (i.e., some operations are missing).
+     */
     override fun actionNonAcceptingTermination(base: String, fsm: DFA) {
         if (fsm.executionTrace.size == 1)
             return // We have not really started yet, so no output here.

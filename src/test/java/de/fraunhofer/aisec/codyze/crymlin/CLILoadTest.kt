5d76771a509288eac63832bd14dc26a98155a30f
Selina Lin <selina.ru-yin.lin@aisec.fraunhofer.de>
2022-01-17 17:52:07 +0100

    implement tests for cli behaviour

---
+++ src/test/java/de/fraunhofer/aisec/codyze/crymlin/CLILoadTest.kt
@@ -0,0 +1,59 @@
+package de.fraunhofer.aisec.codyze.crymlin
+
+import de.fraunhofer.aisec.codyze.analysis.TypestateMode
+import de.fraunhofer.aisec.codyze.config.Configuration
+import java.io.File
+import java.lang.Exception
+import kotlin.Throws
+import kotlin.test.*
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Test
+import picocli.CommandLine
+
+internal class CLILoadTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun noArgsTest() {
+        Assertions.assertThrows(CommandLine.MissingParameterException::class.java) {
+            Configuration.initConfig(null, "")
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun exclusiveOptionTest() {
+        Assertions.assertThrows(CommandLine.MutuallyExclusiveArgsException::class.java) {
+            Configuration.initConfig(null, "-t", "-c")
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun unknownOptionTest() {
+        val config = Configuration.initConfig(null, "-c", "-z")
+        val codyze = config.codyze
+        val cpg = config.cpg
+
+        // assert that nothing was changed from the default values
+        assertTrue(codyze.executionMode.isCli)
+        assertFalse(codyze.executionMode.isLsp)
+        assertFalse(codyze.executionMode.isTui)
+        assertNull(codyze.source)
+        assertContentEquals(arrayOf("./").map { s -> File(s) }.toTypedArray(), codyze.mark)
+        assertEquals("findings.sarif", codyze.output)
+        assertEquals(TypestateMode.DFA, codyze.analysis.tsMode)
+        assertEquals(120L, codyze.timeout)
+        assertFalse(codyze.noGoodFindings)
+        assertFalse(codyze.sarifOutput)
+
+        assertFalse(cpg.translation.analyzeIncludes)
+        assertNull(cpg.translation.includes)
+        assertEquals(0, cpg.additionalLanguages.size, "Set of additional languages is not empty")
+        assertFalse(cpg.useUnityBuild)
+    }
+
+    companion object {
+        @BeforeAll fun startup() {}
+    }
+}

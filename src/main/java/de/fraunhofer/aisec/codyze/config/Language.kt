5927b9ea8fd328e0c85dd6e5d99326b487b24cb0
Selina Lin <selina.ru-yin.lin@aisec.fraunhofer.de>
2022-02-07 17:07:15 +0100

    change Language enum to supply the associated LanguageFrontend and file types

--- src/main/java/de/fraunhofer/aisec/codyze/config/Language.kt
+++ src/main/java/de/fraunhofer/aisec/codyze/config/Language.kt
@@ -1,6 +1,25 @@
 package de.fraunhofer.aisec.codyze.config
 
-enum class Language {
-    PYTHON,
-    GO
+import de.fraunhofer.aisec.cpg.ExperimentalGolang
+import de.fraunhofer.aisec.cpg.ExperimentalPython
+import de.fraunhofer.aisec.cpg.frontends.LanguageFrontend
+import de.fraunhofer.aisec.cpg.frontends.golang.GoLanguageFrontend
+import de.fraunhofer.aisec.cpg.frontends.python.PythonLanguageFrontend
+import java.util.function.Supplier
+
+enum class Language(
+    val frontend: Supplier<Class<out LanguageFrontend?>>,
+    val fileTypes: Supplier<List<String>>
+) {
+    // TODO: Problem is, that cpg go lib is needed, even if you don't need the go frontend
+    @OptIn(ExperimentalPython::class)
+    PYTHON(
+        Supplier<Class<out LanguageFrontend?>> { PythonLanguageFrontend::class.java },
+        Supplier { PythonLanguageFrontend.PY_EXTENSIONS }
+    ),
+    @OptIn(ExperimentalGolang::class)
+    GO(
+        Supplier<Class<out LanguageFrontend?>> { GoLanguageFrontend::class.java },
+        Supplier { GoLanguageFrontend.GOLANG_EXTENSIONS }
+    )
 }

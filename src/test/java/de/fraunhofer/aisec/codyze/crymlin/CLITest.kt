87d578b9a4520064c385e34206f8f1faefbbe1ad
Florian Wendland <florian.wendland@aisec.fraunhofer.de>
2022-02-18 17:16:29 +0100

    Load additional CPG frontends dynamically based on class name (#422)
    Change code referencing frontend classes in CPG directly to dynamically retrieving
    classes based on fully-qualified class name. Prevents compile errors where frontends
    may not be available.

---
+++ src/test/java/de/fraunhofer/aisec/codyze/crymlin/CLITest.kt
@@ -0,0 +1,27 @@
+package de.fraunhofer.aisec.codyze.crymlin
+
+import de.fraunhofer.aisec.codyze.config.Configuration
+import org.junit.jupiter.api.Test
+
+internal class CLITest {
+
+    @Test
+    fun enablePythonSupport() {
+        val cliParameters = arrayOf("-c", "--enable-python-support")
+        val config = Configuration.initConfig(null, *cliParameters)
+        config.buildServerConfiguration()
+
+        // able to handle missing frontends without crashing
+        assert(true)
+    }
+
+    @Test
+    fun useAdditionalLanguagePython() {
+        val cliParameters = arrayOf("-c", "--additional-languages=python")
+        val config = Configuration.initConfig(null, *cliParameters)
+        config.buildServerConfiguration()
+
+        // able to handle missing frontends without crashing
+        assert(true)
+    }
+}

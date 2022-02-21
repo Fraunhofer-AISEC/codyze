88093834bffb844fef6c7385a7f2d32c64420fbe
Robert Haimerl <robert.haimerl@aisec.fraunhofer.de>
2022-02-21 09:57:14 +0100

    added printer classes that write the output

---
+++ src/main/java/de/fraunhofer/aisec/codyze/printer/SarifPrinter.kt
@@ -0,0 +1,23 @@
+package de.fraunhofer.aisec.codyze.printer
+
+import de.fraunhofer.aisec.codyze.analysis.Finding
+import de.fraunhofer.aisec.codyze.sarif.SarifInstantiator
+import java.io.File
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class SarifPrinter(findings: Set<Finding>) : Printer(findings) {
+    var si = SarifInstantiator()
+
+    override var log: Logger = LoggerFactory.getLogger(SarifPrinter::class.java)
+    override var output: String = si.toString()
+
+    override fun printToFile(path: String) {
+        si.generateOutput(File(path))
+        log.info("printed output to file: {}", path)
+    }
+
+    init {
+        si.pushRun(findings)
+    }
+}

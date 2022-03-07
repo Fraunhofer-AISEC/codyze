c9949cf1a7b90bba3a8f3226c4137982d68747c1
Selina Lin <selina.ru-yin.lin@aisec.fraunhofer.de>
2022-03-07 15:51:47 +0100

    add test for pedantic and disabledMarkRules

--- src/test/java/de/fraunhofer/aisec/codyze/crymlin/BuildServerConfigTest.kt
+++ src/test/java/de/fraunhofer/aisec/codyze/crymlin/BuildServerConfigTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 
 internal class BuildServerConfigTest {
     @Test
-    fun enablePedanticTest() {
+    fun pedanticNoGoodFindingsTest() {
         val cliParameters = arrayOf("-c", "--pedantic", "--noGoodFindings")
         val config = Configuration.initConfig(null, *cliParameters)
         val serverConfig = config.buildServerConfiguration()
@@ -14,6 +14,21 @@ internal class BuildServerConfigTest {
         assertTrue(serverConfig.pedantic, "pedantic should be true but was false")
         assertFalse(
             serverConfig.disableGoodFindings,
+            "disableGoodFindings should be false because in pedantic mode all findings should be reported"
+        )
+    }
+
+    @Test
+    fun pedanticDisabledMarkRulesTest() {
+        val cliParameters =
+            arrayOf("-c", "--pedantic", "--disabled-mark-rules=java.UseValidAlgorithm")
+        val config = Configuration.initConfig(null, *cliParameters)
+        val serverConfig = config.buildServerConfiguration()
+
+        assertTrue(serverConfig.pedantic, "pedantic should be true but was false")
+        assertEquals(
+            0,
+            serverConfig.packageToDisabledMarkRules.size,
             "disableGoodFindings should be false because in pedantic mode all findings should be reported"
         )
     }

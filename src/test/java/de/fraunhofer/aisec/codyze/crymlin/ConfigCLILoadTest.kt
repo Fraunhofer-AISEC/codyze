fbb81e96bb8231d80a6750c030659c6e8e5818cb
Selina Lin <74261221+seelchen@users.noreply.github.com>
2022-04-21 13:11:44 +0200

    Fix typos and inconsistencies in help message (#464)
    This PR fixes typos and missing punctuation in the help message. All split characters for list and map type options are changed to the path separation character for each OS (: for Linux/Mac, ; for Windows) for consistency.
    
    Co-authored-by: Florian Wendland <florian.wendland@aisec.fraunhofer.de>

--- src/test/java/de/fraunhofer/aisec/codyze/crymlin/ConfigCLILoadTest.kt
+++ src/test/java/de/fraunhofer/aisec/codyze/crymlin/ConfigCLILoadTest.kt
@@ -25,7 +25,7 @@ class ConfigCLILoadTest {
                 "--sarif", // test if true stays true
                 "--unity", // test if false is set to true
                 "--analyze-includes=false", // test if false is set to false
-                "-m=mark5,mark7,mark6"
+                "-m=mark5${File.pathSeparator}mark7${File.pathSeparator}mark6"
             )
         val config = Configuration.initConfig(correctFile, *options)
         val serverConfig = config.buildServerConfiguration()
@@ -82,9 +82,9 @@ class ConfigCLILoadTest {
             Configuration.initConfig(
                 additionalOptionFile,
                 "-c",
-                "--passes=de.fraunhofer.aisec.cpg.passes.FilenameMapper," +
+                "--passes=de.fraunhofer.aisec.cpg.passes.FilenameMapper${File.pathSeparator}" +
                     "de.fraunhofer.aisec.cpg.passes.CallResolver",
-                "--symbols=&=and,+=plus",
+                "--symbols=&=and${File.pathSeparator}+=plus",
                 "--no-type-system-in-frontend",
                 "--default-passes"
             )

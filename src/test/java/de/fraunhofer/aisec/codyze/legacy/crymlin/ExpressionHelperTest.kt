c4573aba72e319d910a71c896ee8e0bc363418c8
Selina Lin <selina.ru-yin.lin@aisec.fraunhofer.de>
2022-04-28 15:25:23 +0200

    add test for null

--- src/test/java/de/fraunhofer/aisec/codyze/legacy/crymlin/ExpressionHelperTest.kt
+++ src/test/java/de/fraunhofer/aisec/codyze/legacy/crymlin/ExpressionHelperTest.kt
@@ -29,6 +29,16 @@ internal class ExpressionHelperTest {
     }
 
     @Test
+    fun testComparableFloat() {
+        assertEquals("0.0", ExpressionHelper.toComparableString(0.toFloat()))
+        assertEquals("0.0", ExpressionHelper.toComparableString(0.0f))
+        assertEquals("1.2", ExpressionHelper.toComparableString(1.2f))
+        assertEquals("-1.2", ExpressionHelper.toComparableString(-1.2f))
+        assertEquals("1.4E-45", ExpressionHelper.toComparableString(Float.MIN_VALUE))
+        assertEquals("3.4028235E38", ExpressionHelper.toComparableString(Float.MAX_VALUE))
+    }
+
+    @Test
     fun testComparableString() {
         assertEquals("", ExpressionHelper.toComparableString(""))
         assertEquals("a", ExpressionHelper.toComparableString("a"))
@@ -40,8 +50,16 @@ internal class ExpressionHelperTest {
         // assertEquals("ab", ExpressionHelper.toComparableString("ab\""))
         // assertEquals("abc", ExpressionHelper.toComparableString("\"abc\""))
 
-        assertEquals("1.2", ExpressionHelper.toComparableString(1.2f))
         assertEquals("4.0", ExpressionHelper.toComparableString(4.toByte()))
         assertEquals("a", ExpressionHelper.toComparableString('a'))
+    }
+
+    @Test
+    fun testComparableStringObject() {
+        // uses String implementation -> NullPointerException
+        // assertEquals("", ExpressionHelper.toComparableString(null))
+
+        val nullObject: kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType.Object? = null
+        assertEquals("", ExpressionHelper.toComparableString(nullObject))
     }
 }

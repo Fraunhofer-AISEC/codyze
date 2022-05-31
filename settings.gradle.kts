cff9219d0244edd371e938e6d6437f49c7c3e501
Maximilian Galanis <maxi.galanis@gmail.com>
2022-05-31 11:58:48 +0200

    removed modules/cpg from composite build as having the cpg as a normal dependency is sufficient

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,6 @@ rootProject.name = "codyze-composite"
 includeBuild("codyze_v2")
 includeBuild("codyze_v3")
 
-file("modules").listFiles()?.forEach { moduleBuild: File ->
-    includeBuild(moduleBuild)
-}
-
 // TODO: move this into the codyze_legacy build
 gradle.startParameter.excludedTaskNames += ":codyze_v2:licenseMain"
 gradle.startParameter.excludedTaskNames += ":codyze_v2:licenseTest"

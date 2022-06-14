04b45e6b5da362c0e206306f7fa031e746d1f099
Maximilian Galanis <maxi.galanis@gmail.com>
2022-06-14 15:14:19 +0200

    remove unfinished parts from codyze_v3

--- codyze_v3/codyze-core/src/todo/config/TypestateMode.kt
+++
@@ -1,9 +0,0 @@
-package de.fraunhofer.aisec.codyze_core.config
-
-enum class TypestateMode {
-    // Non-deterministic finite automaton. Intraprocedural, not alias-aware.
-    DFA,
-
-    // Weighted Pushdown System. Interprocedural, alias-aware, context-aware.
-    WPDS
-}

20f4dbd2193334e7118953cd5e6dcbf3c06baa2a
Selina Lin <selina.ru-yin.lin@aisec.fraunhofer.de>
2022-06-24 15:29:40 +0200

    add test for combineSources

--- codyze_v3/codyze-core/src/test/kotlin/OptionGroupTest.kt
+++ codyze_v3/codyze-core/src/test/kotlin/OptionGroupTest.kt
@@ -3,6 +3,7 @@ package de.fraunhofer.aisec.codyze_core
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.groups.provideDelegate
 import de.fraunhofer.aisec.codyze_core.config.options.CodyzeOptions
+import de.fraunhofer.aisec.codyze_core.config.options.combineSources
 import kotlin.test.*
 import org.junit.jupiter.api.*
 import org.junit.jupiter.params.ParameterizedTest
@@ -25,24 +26,50 @@ class OptionGroupTest {
         val cli = TestCommand()
         cli.parse(argv)
 
-        val source = cli.codyzeOptions.source.map { p -> p.normalize().absolute().toString() }
+        val mappedSource = cli.codyzeOptions.source.map { p -> p.normalize().absolute().toString() }
+        val mappedExpectedSource = expectedSource.map {  p -> p.normalize().absolute().toString() }
+        assertUnsortedListEquality(mappedExpectedSource, mappedSource)
+    }
 
-        assertEquals(expectedSource.size, source.size, "Size was not equal")
-        for(es in expectedSource.map { p -> p.normalize().absolute().toString() }) {
-            assertContains(source, es)
+
+    @ParameterizedTest
+    @MethodSource("combineSourcesHelper")
+    fun combineSourcesTest(expectedSource: List<Path>, vararg sources: List<Path>) {
+        val combinedSource = combineSources(*sources)
+
+        val mapppedCombinedSource = combinedSource.map { p -> p.normalize().absolute().toString() }
+        val mappedExpectedSource = expectedSource.map { p -> p.normalize().absolute().toString() }
+
+        assertUnsortedListEquality(mappedExpectedSource, mapppedCombinedSource)
+    }
+
+    private fun assertUnsortedListEquality(expected: List<Any>, actual: List<Any>){
+        assertEquals(expected.size, actual.size, "Size was not equal")
+        for(es in expected) {
+            assertContains(actual, es)
         }
-
     }
 
 
     companion object {
-        lateinit var testDir1: Path
-        lateinit var testDir2: Path
-        lateinit var testFile1: Path
+        lateinit var topTestDir: Path
+        private lateinit var testDir1: Path
+        private lateinit var testDir2: Path
+        private lateinit var testFile1: Path
+
+        private lateinit var allFiles: List<Path>
+        private val workingDir: Path = Path(System.getProperty("user.dir"))
 
         @BeforeAll
         @JvmStatic
         fun startup() {
+            val topTestDirResource =
+                OptionGroupTest::class
+                    .java.classLoader.getResource("cli-test-directory")
+            assertNotNull(topTestDirResource)
+            topTestDir = Path(topTestDirResource.path)
+            assertNotNull(topTestDir)
+
             val testDir1Resource =
                 OptionGroupTest::class
                     .java.classLoader.getResource("cli-test-directory/dir1")
@@ -63,11 +90,24 @@ class OptionGroupTest {
             assertNotNull(testFile1Resource)
             testFile1 = Path(testFile1Resource.path)
             assertNotNull(testFile1)
+
+            allFiles =  listOf(
+                testFile1,
+                Path(topTestDir.toString(), "file2.java"),
+                Path(testDir1.toString(),"dir1file1.java"),
+                Path(testDir2.toString(),"dir2dir1","dir2dir1file1.java"),
+                Path(testDir2.toString(),"dir2dir1","dir2dir1file2.java"),
+                Path(testDir2.toString(),"dir2dir2","dir2dir2file1.java"),
+                Path(testDir2.toString(),"dir2dir3","dir2dir3file1.java"),
+                Path(testDir2.toString(),"dir2file1.java"),
+                Path(testDir2.toString(),"dir2file2.java")
+            )
         }
 
         @JvmStatic
         fun sourceParamHelper(): Stream<Arguments> {
             return Stream.of(
+                // Tests if source and source-additions are combined correctly
                 Arguments.of(
                     arrayOf(
                         "--source", testDir1.toString(),
@@ -86,6 +126,7 @@ class OptionGroupTest {
                         Path(testDir2.toString(),"dir2file2.java")
                     )
                 ),
+                // Tests if disabled-source files are correctly removed from source
                 Arguments.of(
                     arrayOf(
                         "--source", testDir1.toString(),
@@ -101,21 +142,67 @@ class OptionGroupTest {
                         Path(testDir2.toString(),"dir2file1.java"),
                         Path(testDir2.toString(),"dir2file2.java")
                     )
-                ),
-                Arguments.of(
-                    arrayOf(
-                        "-s", testDir1.toString(),
-                        "-s", Path(
-                            testDir1.toString(),
-                            testDir1.relativize(Path(System.getProperty("user.dir"))).toString(),
-                            Path(System.getProperty("user.dir")).relativize(testDir1).toString(),
-                            "dir1file1.java").toString(),
-                        "--spec", testDir1.toString()
-                    ),
-                    listOf(
-                        Path(testDir1.toString(),"dir1file1.java")
-                    )
                 )
+            )
+        }
+
+        @JvmStatic
+        fun combineSourcesHelper(): Stream<Arguments> {
+            return Stream.of(
+                // Tests if duplicates are filtered out
+                Arguments.of(
+                    allFiles,
+                    arrayOf(
+                        listOf(
+                            testFile1,
+                            Path(testDir1.toString(),"dir1file1.java")),
+                        listOf(
+                            topTestDir,
+                            testFile1
+                        ),
+                        listOf(
+                            testDir1
+                        ),
+                        listOf(
+                            testDir2,
+                            topTestDir,
+                            Path(testDir2.toString(), "dir2dir1")
+                        )
+                    )
+                ),
+                // Tests if normalization works to filter out duplicates
+                Arguments.of(
+                    listOf(
+                        testFile1
+                    ),
+                    arrayOf(
+                        listOf(
+                            Path(
+                                testDir1.toString(),
+                                testDir1.relativize(workingDir).toString(),
+                                workingDir.relativize(testDir1).toString(),
+                                testDir1.relativize(topTestDir).toString(),
+                                topTestDir.relativize(testFile1).toString()
+                            ),
+                            testFile1
+                        )
+                    )
+
+                ),
+                // test is relative paths are resolved correctly
+                Arguments.of(
+                    listOf(
+                        Path(testDir1.toString(), "dir1file1.java")
+                    ),
+                    arrayOf(
+                        listOf(
+                            workingDir.relativize(testDir1),
+                            testDir1
+                        )
+                    )
+
+                )
+
             )
         }
     }

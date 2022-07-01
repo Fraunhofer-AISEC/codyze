58a07870fa0fc2be23a73eb921a90a784720f225
maximilian-galanis <maxi.galanis@gmail.com>
2022-07-01 16:23:22 +0200

    Sl/common options (#519)
    * move common options in a base class (CodyzeSubcommand)
    
    Co-authored-by: Selina Lin <selina.ru-yin.lin@aisec.fraunhofer.de>

--- codyze-v3/codyze/src/main/kotlin/SubcommandInteractive.kt
+++ codyze-v3/codyze/src/main/kotlin/SubcommandInteractive.kt
@@ -1,21 +1,12 @@
 package de.fraunhofer.aisec.codyze
 
-import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.groups.provideDelegate
-import de.fraunhofer.aisec.codyze.options.*
-import java.nio.file.Path
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
-class Interactive : CliktCommand("Start an interactive console") {
-    // This is only here to correctly display the help message
-    private val unusedConfigFile: Path by configFileOption()
+class Interactive : CodyzeSubcommand("Start an interactive console") {
+    // possibly add subcommand-interactive specific options here
 
-    val codyzeOptions by CodyzeOptions()
-    val analysisOptions by AnalysisOptions()
-    val cpgOptions by CPGOptions()
-    val translationOptions by TranslationOptions()
     override fun run() {
         logger.debug { "Executing 'interactive' subcommand..." }
         echo("When I grow up, I'll start an interactive console!")

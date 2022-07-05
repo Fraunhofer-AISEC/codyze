0f59dc11c5618bed1dab3241d2475dd9ab3f262f
Maximilian Galanis <maximilian.galanis@aisec.fraunhofer.de>
2022-07-05 14:12:25 +0200

    add docstrings

--- codyze-v3/codyze-core/src/main/kotlin/ProjectServer.kt
+++ codyze-v3/codyze-core/src/main/kotlin/ProjectServer.kt
@@ -16,37 +16,42 @@ object ProjectServer {
     /** All built-in executors that are available for the analysis. */
     var executors: List<Executor> = emptyList() // initialized in <registerExecutors>
 
-    /**
-     * Initialize the CPG, the available executors and populate the CPG with source code files.
-     *
-     * 1. load source code (TODO)
-     * 2. initialize CPG with given source code (TODO)
-     * 3. initialize executors
-     */
     init {
         registerExecutors()
     }
 
     /**
-     * Run once when first initializing the AnalysisServer.
+     * Run once when first initializing the [ProjectServer].
      *
-     * Uses Koin (a DI library) to get access to all executors
+     * Uses Koin (a DI library) to get access to all built-in [Executor]s
      */
     private fun registerExecutors() {
         executors = getKoin().getAll<Executor>()
         logger.debug {
-            "Found executors for following file types: ${executors.flatMap { it.supportedFileExtensions }}"
+            "Found executors for following file types: ${executors.map { it.supportedFileExtension }}"
         }
     }
 
     /**
-     * Returns the project associated with the given [Configuration] object.
+     * Connect to the project associated with the given [Configuration] object.
      *
-     * If no such project exists, a new project with this configuration is added to [projects].
+     * If this project does not exist, a new project from this [Configuration] is added to the list
+     * of open [projects].
+     *
+     * @param config [Configuration] to get a [Project] from
+     * @return [Project] associated with the given [Configuration]
      */
     fun connect(config: Configuration): Project {
-        // TODO: remove 'unused' projects from this.projects
         // if project exists -> "reload" else create new project
         return projects.getOrPut(config) { Project(config) }
+    }
+
+    /**
+     * Disconnect from the [Project] associated with the given [Configuration] object.
+     *
+     * If no [Project] for the given [Configuration] exists, this method does nothing.
+     */
+    fun disconnect(config: Configuration) {
+        projects.remove(config)
     }
 }

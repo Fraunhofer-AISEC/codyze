2e7cebfb2d9d7505e798bf5fa08b3c054b676cea
Wendland, Florian <florian.wendland@aisec.fraunhofer.de>
2022-08-22 13:55:04 +0200

    Fix links to sources

--- codyze-v3/buildSrc/src/main/kotlin/documented.gradle.kts
+++ codyze-v3/buildSrc/src/main/kotlin/documented.gradle.kts
@@ -10,7 +10,8 @@ tasks.withType<DokkaTaskPartial>().configureEach {
         configureEach {
             sourceLink {
                 localDirectory.set(file("src/main/kotlin"))
-                remoteUrl.set(URL("https://github.com/Fraunhofer-AISEC/codyze"))
+                // ensure we're including path segment to module
+                remoteUrl.set(URL("https://github.com/Fraunhofer-AISEC/codyze/blob/main/codyze-v3/${project.projectDir.relativeTo(project.rootDir)}/src/main/kotlin"))
                 remoteLineSuffix.set("#L")
             }
         }

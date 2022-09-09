95a040e6f2b4bd2edcbe3755d5cc72f9c9d7a917
Maximilian Galanis <maximilian.galanis@aisec.fraunhofer.de>
2022-09-09 11:52:14 +0200

    add option to add the cpg as an included build to codyze-v3

--- codyze-v3/settings.gradle.kts
+++ codyze-v3/settings.gradle.kts
@@ -2,6 +2,20 @@ rootProject.name = "codyze-v3"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
+/*
+ enable this if you want to develop codyze and the cpg together
+
+ for this to work you have to slightly adapt the cpg project because of a bug in gradle concerning
+ the "TYPESAFE_PROJECT_ACCESSORS" and submodules with the same name as the root project
+ see: https://github.com/gradle/gradle/issues/16608
+  - rename the cpg/cpg folder to cpg/cpg-main (or anything other than cpg really)
+  - change all usages of the cpg/cpg folder to cpg/cpg-main:
+      - in the cpg/cpg-main/build.gradle.kts file: replace "cpg" with "cpg-main"
+      - in the cpg/settings.gradle.kts file: replace ":cpg" with ":cpg-main"
+ */
+// includeBuild("relative path to the cpg project")  // e.g., "../../cpg"
+
+
 include("codyze")
 include("codyze-core")
 

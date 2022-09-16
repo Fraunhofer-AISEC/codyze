1c28697a2a01f11afcbd4bcebb0c8e664a4ea205
Maximilian Galanis <maximilian.galanis@aisec.fraunhofer.de>
2022-09-16 13:09:54 +0200

    remove nwt-example submodule now that coko is a working prototype

--- codyze-v3/codyze-specification-languages/nwt-example/src/main/kotlin/nwt/domain/Account.kt
+++
@@ -1,3 +0,0 @@
-package de.fraunhofer.aisec.codyze.specification_languages.nwt.domain
-
-data class Account(val id: String, val name: String)
--- codyze-v3/codyze-specification-languages/nwt-example/src/main/kotlin/nwt/dsl/nwt.kt
+++
@@ -1,5 +0,0 @@
-package de.fraunhofer.aisec.codyze.specification_languages.nwt.dsl
-
-fun nwt(initialize: NwtBuilder.() -> Unit) {
-    NwtBuilder().apply(initialize)
-}

8d838747c178e8d9f346b67f847c0570e74336a2
Wendland, Florian <florian.wendland@aisec.fraunhofer.de>
2022-09-27 11:25:44 +0200

    Configure Dokka tasks for v3
    - Need partial for when we build our multi module docs for the website
    - Need task for when we build docs for each module individually (e.g.
      when publishing docs as consumable artefact)

--- codyze-v3/build.gradle.kts
+++ codyze-v3/build.gradle.kts
@@ -1,12 +1,44 @@
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.base.DokkaBaseConfiguration
+
 allprojects {
     group = "de.fraunhofer.aisec.codyze"
-    version = if(version != Project.DEFAULT_VERSION) version else "0.0.0-SNAPSHOT"
+    version = if (version != Project.DEFAULT_VERSION) version else "0.0.0-SNAPSHOT"
 }
 
 plugins {
     id("documented")
 }
 
+// generate API documentation for website
 tasks.dokkaHtmlMultiModule.configure {
-    outputDirectory.set(projectDir.resolve("..").resolve("docs").resolve("api").resolve("codyze-v3"))
+    // add API docs to directory used for website generation
+    outputDirectory.set(
+        project.rootDir.resolve("..").resolve("docs").resolve("api").resolve("codyze-v3")
+    )
+    // path to Dokka assets
+    val dokkaAssetsBaseDirectory =
+        project.rootDir.resolve("..").resolve("docs").resolve("assets").resolve("dokka")
+    // configure custom assets
+    pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+        // use custom stylesheets without external content
+        customStyleSheets =
+            listOf(
+                dokkaAssetsBaseDirectory.resolve("style.css"),
+                dokkaAssetsBaseDirectory.resolve("jetbrains-mono.css"),
+            )
+    }
+    // copy over font files
+    dokkaAssetsBaseDirectory
+        .resolve("JetBrainsMono")
+        .copyRecursively(
+            target = outputDirectory.get().resolve("styles").resolve("JetBrainsMono"),
+            overwrite = true,
+        )
+    dokkaAssetsBaseDirectory
+        .resolve("inter")
+        .copyRecursively(
+            target = outputDirectory.get().resolve("styles").resolve("inter"),
+            overwrite = true,
+        )
 }
--- codyze-v3/buildSrc/src/main/kotlin/documented.gradle.kts
+++ codyze-v3/buildSrc/src/main/kotlin/documented.gradle.kts
@@ -1,23 +1,59 @@
-import org.jetbrains.dokka.gradle.DokkaTaskPartial
 import java.net.URL
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.base.DokkaBaseConfiguration
+import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.dokka.gradle.DokkaTaskPartial
 
 plugins {
     id("org.jetbrains.dokka")
 }
 
+repositories {
+    mavenCentral()
+}
+
+// configure partial generation for multi module
 tasks.withType<DokkaTaskPartial>().configureEach {
     dokkaSourceSets {
         configureEach {
             sourceLink {
                 localDirectory.set(file("src/main/kotlin"))
                 // ensure we're including path segment to module
-                remoteUrl.set(URL("https://github.com/Fraunhofer-AISEC/codyze/blob/main/codyze-v3/${project.projectDir.relativeTo(project.rootDir)}/src/main/kotlin"))
+                remoteUrl.set(
+                    URL(
+                        "https://github.com/Fraunhofer-AISEC/codyze/blob/main/codyze-v3/${project.projectDir.relativeTo(project.rootDir)}/src/main/kotlin"
+                    )
+                )
                 remoteLineSuffix.set("#L")
             }
         }
     }
 }
 
-repositories {
-    mavenCentral()
+// configure task for individual module
+tasks.withType<DokkaTask>().configureEach {
+    // path to Dokka assets
+    val dokkaAssetsBaseDirectory = project.rootDir.resolve("..").resolve("docs").resolve("assets").resolve("dokka")
+    // configure custom assets
+    pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+        // use custom stylesheets without external content
+        customStyleSheets =
+            listOf(
+                dokkaAssetsBaseDirectory.resolve("style.css"),
+                dokkaAssetsBaseDirectory.resolve("jetbrains-mono.css"),
+            )
+    }
+    // copy over font files
+    dokkaAssetsBaseDirectory
+        .resolve("JetBrainsMono")
+        .copyRecursively(
+            target = outputDirectory.get().resolve("styles").resolve("JetBrainsMono"),
+            overwrite = true,
+        )
+    dokkaAssetsBaseDirectory
+        .resolve("inter")
+        .copyRecursively(
+            target = outputDirectory.get().resolve("styles").resolve("inter"),
+            overwrite = true,
+        )
 }

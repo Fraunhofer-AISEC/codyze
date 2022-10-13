a779c111ba2a749391643545296d3ffaa977dbdb
Maximilian Galanis <maximilian.galanis@aisec.fraunhofer.de>
2022-10-13 08:21:11 +0200

    move Nodes typealias out of dsl package -> using OpDsl is the preferred approach for coko now

---
+++ codyze-v3/codyze-specification-languages/coko/coko-core/src/main/kotlin/Types.kt
@@ -0,0 +1,5 @@
+package de.fraunhofer.aisec.codyze.specification_languages.coko.coko_core
+
+import de.fraunhofer.aisec.cpg.graph.Node
+
+typealias Nodes = Collection<Node>
--- codyze-v3/codyze-specification-languages/coko/coko-core/src/main/kotlin/dsl/Types.kt
+++ codyze-v3/codyze-specification-languages/coko/coko-core/src/main/kotlin/dsl/Types.kt
@@ -2,14 +2,10 @@
 
 package de.fraunhofer.aisec.codyze.specification_languages.coko.coko_core.dsl
 
-import de.fraunhofer.aisec.cpg.graph.Node
-
 /** Matches any value. */
 object Wildcard
 
 typealias wildcard = Wildcard
-
-typealias Nodes = Collection<Node>
 
 /** Stores the fully qualified name of a class */
 data class Type(val fqn: String)

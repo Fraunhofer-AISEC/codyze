a1c1d723ba9eff4cf819feaf1e0d2e591a5d57fd
Maximilian Galanis <maximilian.galanis@aisec.fraunhofer.de>
2022-10-13 08:18:53 +0200

    update follows evaluator to work with Op objects instead of Nodes

--- codyze-v3/codyze-specification-languages/coko/coko-core/src/main/kotlin/dsl/Evaluators.kt
+++ codyze-v3/codyze-specification-languages/coko/coko-core/src/main/kotlin/dsl/Evaluators.kt
@@ -3,14 +3,16 @@
 package de.fraunhofer.aisec.codyze.specification_languages.coko.coko_core.dsl
 
 import de.fraunhofer.aisec.codyze.specification_languages.coko.coko_core.Project
+import de.fraunhofer.aisec.codyze.specification_languages.coko.coko_core.modelling.getNodes
 import de.fraunhofer.aisec.codyze.specification_languages.coko.coko_core.ordering.Order
-import de.fraunhofer.aisec.cpg.graph.*
 import de.fraunhofer.aisec.cpg.query.executionPath
 
 context(Project)
 /** For each of the nodes in [this], there is a path to at least one of the nodes in [that]. */
-infix fun Collection<Node>.follows(that: Collection<Node>): Boolean {
-    return this.all { from -> that.any { to -> executionPath(from, to).value } }
+infix fun Op.follows(that: Op): Boolean {
+    val thisNodes = getNodes()
+    val thatNodes = that.getNodes()
+    return thisNodes.all { from -> thatNodes.any { to -> executionPath(from, to).value } }
 }
 
 context(Project)

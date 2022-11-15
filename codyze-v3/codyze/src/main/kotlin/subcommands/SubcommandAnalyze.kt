974eef82c98a450f1c7ec14650983a5dca7d12f5
Maximilian Galanis <maximilian.galanis@aisec.fraunhofer.de>
2022-11-15 16:49:23 +0100

    add new 'timed' function to simplify logging of measured times of code runtime

--- codyze-v3/codyze/src/main/kotlin/subcommands/SubcommandAnalyze.kt
+++ codyze-v3/codyze/src/main/kotlin/subcommands/SubcommandAnalyze.kt
@@ -1,12 +1,9 @@
 package de.fraunhofer.aisec.codyze.subcommands
 
 import de.fraunhofer.aisec.codyze.CodyzeSubcommand
-import de.fraunhofer.aisec.codyze_core.Project
 import de.fraunhofer.aisec.codyze_core.ProjectServer
 import de.fraunhofer.aisec.codyze_core.config.buildConfiguration
-import kotlin.time.Duration
-import kotlin.time.ExperimentalTime
-import kotlin.time.measureTimedValue
+import de.fraunhofer.aisec.codyze_core.timed
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import mu.KotlinLogging
@@ -17,22 +14,17 @@ private val logger = KotlinLogging.logger {}
 class Analyze : CodyzeSubcommand("Analyze a set of source files") {
     // possibly add subcommand-analyze specific options here
 
-    @OptIn(ExperimentalTime::class)
     override fun run() {
         logger.debug { "Executing 'analyze' subcommand..." }
 
-        val (project: Project, projectServerDuration: Duration) =
-            measureTimedValue {
-                ProjectServer.connect(
-                    config =
-                        buildConfiguration(
-                            codyzeConfigurationRegister,
-                            backendConfigurationRegister
-                        )
+        val project = timed("Starting project server took") {
+            ProjectServer.connect(
+                config =
+                buildConfiguration(
+                    codyzeConfigurationRegister,
+                    backendConfigurationRegister
                 )
-            }
-        logger.debug {
-            "Project server started in ${ projectServerDuration.inWholeMilliseconds } ms"
+            )
         }
 
         val result = project.doStuff()

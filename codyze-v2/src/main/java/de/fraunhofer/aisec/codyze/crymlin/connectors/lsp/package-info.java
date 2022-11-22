c53547b59c1532fdcbddb3fe3380d5e3ff777219
Wendland, Florian <florian.wendland@aisec.fraunhofer.de>
2022-11-22 16:46:21 +0100

    Remove v2 from main
    We're archiving v2 in favor of our reimplementation in v3. All v2 code
    remains as tagged version and maybe as branch.

--- codyze-v2/src/main/java/de/fraunhofer/aisec/codyze/crymlin/connectors/lsp/package-info.java
+++
@@ -1,3 +0,0 @@
-/** Language Server Protocol (LSP) Connector */
-
-package de.fraunhofer.aisec.codyze.crymlin.connectors.lsp;
--- codyze-v2/src/test/resources/java/jca/include/SecretKey.java
+++
@@ -1,7 +0,0 @@
-package javax.crypto;
-
-import java.security.Key;
-
-public class SecretKey extends Key {
-
-}

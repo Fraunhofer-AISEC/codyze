cc7c0bb34063c99d95142991bfb6ca266a00319a
Maximilian Galanis <maximilian.galanis@aisec.fraunhofer.de>
2022-11-29 14:09:33 +0100

    move license header to file and activate license checking by detekt

--- buildSrc/src/main/kotlin/code-quality.gradle.kts
+++ buildSrc/src/main/kotlin/code-quality.gradle.kts
@@ -23,37 +23,6 @@ tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
 }
 
-val header = """
-/*
- * Copyright (c) ${"$"}YEAR, Fraunhofer AISEC. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *     ${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}\                  ${'$'}${'$'}\
- *    ${'$'}${'$'}  __${'$'}${'$'}\                 ${'$'}${'$'} |
- *    ${'$'}${'$'} /  \__| ${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}\   ${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}${'$'} |${'$'}${'$'}\   ${'$'}${'$'}\ ${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}\  ${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}\
- *    ${'$'}${'$'} |      ${'$'}${'$'}  __${'$'}${'$'}\ ${'$'}${'$'}  __${'$'}${'$'} |${'$'}${'$'} |  ${'$'}${'$'} |\____${'$'}${'$'}  |${'$'}${'$'}  __${'$'}${'$'}\
- *    ${'$'}${'$'} |      ${'$'}${'$'} /  ${'$'}${'$'} |${'$'}${'$'} /  ${'$'}${'$'} |${'$'}${'$'} |  ${'$'}${'$'} |  ${'$'}${'$'}${'$'}${'$'} _/ ${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}${'$'} |
- *    ${'$'}${'$'} |  ${'$'}${'$'}\ ${'$'}${'$'} |  ${'$'}${'$'} |${'$'}${'$'} |  ${'$'}${'$'} |${'$'}${'$'} |  ${'$'}${'$'} | ${'$'}${'$'}  _/   ${'$'}${'$'}   ____|
- *    \${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}  |\${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}  |\${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}${'$'} |\${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}${'$'} |${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}\ \${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}\
- *     \______/  \______/  \_______| \____${'$'}${'$'} |\________| \_______|
- *                                  ${'$'}${'$'}\   ${'$'}${'$'} |
- *                                  \${'$'}${'$'}${'$'}${'$'}${'$'}${'$'}  |
- *                                   \______/
- *
- */
-""".trimIndent()
-
 detekt {
     config = files("$rootDir/detekt.yml")
 }
@@ -73,7 +42,7 @@ tasks.withType<Detekt> detekt@{ // Sadly it has to be eager.
 // for now we use spotless just for the license headers
 spotless {
     kotlin {
-        licenseHeader(header).yearSeparator(" - ")
+        licenseHeaderFile("$rootDir/buildSrc/src/main/resources/licenseHeader.txt")
         targetExclude("**/*.codyze.kts")
     }
 }

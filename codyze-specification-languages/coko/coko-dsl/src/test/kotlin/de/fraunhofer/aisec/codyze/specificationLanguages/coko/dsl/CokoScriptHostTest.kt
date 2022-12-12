3eca74210e89b67168cb0b5df206b43d1f1f394c
Maximilian Galanis <maximilian.galanis@aisec.fraunhofer.de>
2022-12-12 12:56:44 +0100

    test codecov workflow with failing test in coko-dsl

--- codyze-specification-languages/coko/coko-dsl/src/test/kotlin/de/fraunhofer/aisec/codyze/specificationLanguages/coko/dsl/CokoScriptHostTest.kt
+++ codyze-specification-languages/coko/coko-dsl/src/test/kotlin/de/fraunhofer/aisec/codyze/specificationLanguages/coko/dsl/CokoScriptHostTest.kt
@@ -33,7 +33,7 @@ class CokoScriptHostTest {
             CokoExecutor.eval(
                 """
                 interface TestInterface {
-                    fun log(message: String)
+                    fun log(message: String): Ops
                 }
                 """.trimIndent(),
                 mockk()

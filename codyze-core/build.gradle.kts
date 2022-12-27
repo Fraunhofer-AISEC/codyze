e5ba6def19887cf8931d526411c44f27128f2a7f
Wendland, Florian <florian.wendland@aisec.fraunhofer.de>
2022-12-27 09:42:50 +0100

    Add kotlinx serialization dependency for sarif4k

--- codyze-core/build.gradle.kts
+++ codyze-core/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     // The code can be found here: https://github.com/detekt/sarif4k
     // The code in it was generated using https://app.quicktype.io/ with minor manual additions
     implementation(libs.sarif4k)
+    implementation(libs.kotlinx.serialization.json) // dependency for sarif4k
 }
 
 // run the 'projectProps' task when the processResources task is run

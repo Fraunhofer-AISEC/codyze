d514843d11a14c676d53207f580625f2def9a118
Selina Lin <selina.ru-yin.lin@aisec.fraunhofer.de>
2023-09-08 16:25:24 +0200

    change CpgWheneverEvaluator to be able to give more info in findings

--- codyze-backends/cpg/src/main/kotlin/de/fraunhofer/aisec/codyze/backends/cpg/coko/evaluators/CpgWheneverEvaluator.kt
+++ codyze-backends/cpg/src/main/kotlin/de/fraunhofer/aisec/codyze/backends/cpg/coko/evaluators/CpgWheneverEvaluator.kt
@@ -65,8 +65,10 @@ class CpgWheneverEvaluator(premise: ConditionComponent) : WheneverEvaluator(prem
         } else {
             for (premiseNode in premiseNodes) {
                 val ensuresToNodes = ensures.associateWith { ensure ->
-                    val (ensureNodes, problems) = evaluateConditionComponentToCpgNodes(ensure, premiseNode)
-                    ensureNodes.filterNot { it is DummyNode } to problems
+                    evaluateConditionComponentToCpgNodes(
+                        ensure,
+                        premiseNode
+                    ).removeDummyNodes()
                 }
                 val callAssertionsToNodes = callAssertions.associateWith { callAssertion ->
                     val nodes = callAssertion.op.cpgGetNodes()
@@ -83,7 +85,8 @@ class CpgWheneverEvaluator(premise: ConditionComponent) : WheneverEvaluator(prem
                             Scope.Block -> TODO()
                         }
                     }
-                    nodes.toList() to Problems()
+                    TODO()
+                    EvaluationResult(nodes, emptyList(), Problems())
                 }
                 findings.addAll(
                     generateFindings(premiseNode, ensuresToNodes + callAssertionsToNodes, passMessage, failMessage)
@@ -96,32 +99,34 @@ class CpgWheneverEvaluator(premise: ConditionComponent) : WheneverEvaluator(prem
 
     private fun generateFindings(
         premiseNode: Node,
-        conditionToNodes: Map<ConditionComponent, Pair<List<Node>, Problems>>,
+        conditionToNodes: Map<ConditionComponent, EvaluationResult>,
         passMessage: String,
         failMessage: String
     ): Collection<Finding> {
         val findings = mutableListOf<Finding>()
 
-        val unfulfilledConditions = conditionToNodes.filterValues { (nodes, _) -> nodes.isEmpty() }
+        val unfulfilledConditions = conditionToNodes.filterValues { (fulfilled, _, _) -> fulfilled.isEmpty() }
 
         if (unfulfilledConditions.isNotEmpty()) {
             for ((condition, nodesToProblems) in unfulfilledConditions) {
-                val (_, problems) = nodesToProblems
-                if (problems.isEmpty()) {
-                    findings.add(
-                        CpgFinding(
-                            message = "No $condition found around $premise. $failMessage",
-                            kind = Finding.Kind.Fail,
-                            node = premiseNode
-                        )
-                    )
-                    TODO("Better message")
-                } else {
+                val (_, unfulfilled, problems) = nodesToProblems
+                if (problems.isNotEmpty()) {
                     findings.add(
                         CpgFinding(
                             message = "There were problems in resolving $condition. $problems",
                             kind = Finding.Kind.Open,
                             node = premiseNode,
+                            relatedNodes = problems.getNodes()
+                        )
+                    )
+                } else {
+                    // TODO: Better message
+                    findings.add(
+                        CpgFinding(
+                            message = "No $condition found around $premise. $failMessage",
+                            kind = Finding.Kind.Fail,
+                            node = premiseNode,
+                            relatedNodes = unfulfilled
                         )
                     )
                 }
@@ -132,9 +137,21 @@ class CpgWheneverEvaluator(premise: ConditionComponent) : WheneverEvaluator(prem
                     message = passMessage,
                     kind = Finding.Kind.Pass,
                     node = premiseNode,
-                    relatedNodes = conditionToNodes.values.flatMap { it.first }
+                    relatedNodes = conditionToNodes.values.flatMap { it.fulfillingNodes }
                 )
             )
+
+            val conditionToProblems = conditionToNodes.mapValues { (_, result) -> result.problems }
+            for ((condition, problems) in conditionToProblems) {
+                if (problems.isNotEmpty()) {
+                    CpgFinding(
+                        message = "There were problems in resolving $condition. $problems",
+                        kind = Finding.Kind.Informational,
+                        node = premiseNode,
+                        relatedNodes = problems.getNodes()
+                    )
+                }
+            }
         }
 
         return findings
@@ -143,8 +160,7 @@ class CpgWheneverEvaluator(premise: ConditionComponent) : WheneverEvaluator(prem
     private fun evaluateConditionNodeToCpgNodes(
         conditionNode: ConditionNode,
         premiseNode: Node? = null
-    ): Pair<Nodes, Problems> {
-        TODO("change to EvaluationResult")
+    ): EvaluationResult {
         return when (conditionNode) {
             is ConditionComponent -> evaluateConditionComponentToCpgNodes(conditionNode, premiseNode)
             is ArgumentItem<*> -> evaluateArgumentItemToCpgNodes(conditionNode, premiseNode)
@@ -153,30 +169,30 @@ class CpgWheneverEvaluator(premise: ConditionComponent) : WheneverEvaluator(prem
         }
     }
 
-    private fun evaluateValueToCpgNodes(value: Value<*>, premiseNode: Node? = null): Pair<Nodes, Problems> {
-        return listOf(DummyNode()) to Problems()
+    private fun evaluateValueToCpgNodes(value: Value<*>, premiseNode: Node? = null): EvaluationResult {
+        return EvaluationResult(listOf(DummyNode()), emptyList(), Problems())
     }
 
     private fun evaluateArgumentItemToCpgNodes(
         argumentItem: ArgumentItem<*>,
         premiseNode: Node? = null
-    ): Pair<Nodes, Problems> {
+    ): EvaluationResult {
         val nodes = argumentItem.cpgGetNodes().filterWithDistanceToPremise(premiseNode)
-        return nodes to Problems()
+        return EvaluationResult(nodes, emptyList(), Problems())
     }
 
     private fun evaluateReturnValueItemToCpgNodes(
         returnValueItem: ReturnValueItem<*>,
         premiseNode: Node? = null
-    ): Pair<Nodes, Problems> {
+    ): EvaluationResult {
         val nodes = returnValueItem.cpgGetNodes().filterWithDistanceToPremise(premiseNode)
-        return nodes to Problems()
+        return EvaluationResult(nodes, emptyList(), Problems())
     }
 
     private fun evaluateConditionComponentToCpgNodes(
         conditionComponent: ConditionComponent,
         premiseNode: Node? = null
-    ): Pair<Nodes, Problems> {
+    ): EvaluationResult {
         return when (conditionComponent) {
             is BinaryLogicalConditionComponent ->
                 evaluateBinaryLogicalConditionComponent(conditionComponent, premiseNode)
@@ -190,74 +206,97 @@ class CpgWheneverEvaluator(premise: ConditionComponent) : WheneverEvaluator(prem
     private fun evaluateBinaryLogicalConditionComponent(
         binaryLogicalConditionComponent: BinaryLogicalConditionComponent,
         premiseNode: Node? = null
-    ): Pair<Nodes, Problems> {
-        val (leftNodes, leftProblems) =
+    ): EvaluationResult {
+        val (leftFulfillingNodes, leftUnfulfillingNodes, leftProblems) =
             evaluateConditionComponentToCpgNodes(binaryLogicalConditionComponent.left, premiseNode)
-        val (rightNodes, rightProblems) =
+        val (rightFulfillingNodes, rightUnfulfillingNodes, rightProblems) =
             evaluateConditionComponentToCpgNodes(binaryLogicalConditionComponent.right, premiseNode)
         return when (binaryLogicalConditionComponent.operator) {
-            BinaryLogicalOperatorName.AND -> (leftNodes intersect rightNodes) to (leftProblems + rightProblems)
-            BinaryLogicalOperatorName.OR -> (leftNodes union rightNodes) to (leftProblems + rightProblems)
+            BinaryLogicalOperatorName.AND ->
+                EvaluationResult(
+                    leftFulfillingNodes intersect rightFulfillingNodes.toSet(),
+                    leftUnfulfillingNodes union rightUnfulfillingNodes,
+                    leftProblems + rightProblems
+                )
+            BinaryLogicalOperatorName.OR ->
+                EvaluationResult(
+                    leftFulfillingNodes union rightFulfillingNodes,
+                    leftUnfulfillingNodes union rightUnfulfillingNodes,
+                    leftProblems + rightProblems
+                )
         }
     }
 
     private fun evaluateComparisonConditionComponent(
         comparisonConditionComponent: ComparisonConditionComponent,
         premiseNode: Node? = null
-    ): Pair<Nodes, Problems> {
-        val (leftNodes, leftProblems) =
+    ): EvaluationResult {
+        val (leftFulfillingNodes, leftUnfulfillingNodes, leftProblems) =
             evaluateConditionNodeToCpgNodes(comparisonConditionComponent.left, premiseNode)
-        val (rightNodes, rightProblems) =
+        val (rightFulfillingNodes, rightUnfulfillingNodes, rightProblems) =
             evaluateConditionNodeToCpgNodes(comparisonConditionComponent.right, premiseNode)
 
         val newProblems = Problems()
-        val result = mutableSetOf<Node>()
+        val newFulfillingNodes = mutableSetOf<Node>()
+        val newUnfulfillingNodes = mutableSetOf<Node>()
 
         val leftNodesToValues =
-            leftNodes.toNodesToValues(newProblems, comparisonConditionComponent.left.transformation)
+            leftFulfillingNodes.toNodesToValues(newProblems, comparisonConditionComponent.left.transformation)
         val rightNodesToValues =
-            rightNodes.toNodesToValues(newProblems, comparisonConditionComponent.right.transformation)
+            rightFulfillingNodes.toNodesToValues(newProblems, comparisonConditionComponent.right.transformation)
 
         for ((leftNode, leftValue) in leftNodesToValues) {
-            for ((rightNode, rightValue) in rightNodesToValues)
-                when (comparisonConditionComponent.operator) {
-                    ComparisonOperatorName.GEQ -> TODO()
-                    ComparisonOperatorName.GT -> TODO()
-                    ComparisonOperatorName.LEQ -> TODO()
-                    ComparisonOperatorName.LT -> TODO()
-                    ComparisonOperatorName.EQ ->
-                        if (leftValue == rightValue) {
-                            result.add(leftNode)
-                            result.add(rightNode)
-                        }
-                    ComparisonOperatorName.NEQ -> TODO()
+//            for ((rightNode, rightValue) in rightNodesToValues)
+            when (comparisonConditionComponent.operator) {
+                ComparisonOperatorName.GEQ -> TODO()
+                ComparisonOperatorName.GT -> TODO()
+                ComparisonOperatorName.LEQ -> TODO()
+                ComparisonOperatorName.LT -> TODO()
+                ComparisonOperatorName.EQ -> {
+                    val (fulfilling, unfulfilling) =
+                        rightNodesToValues.toList().partition { (_, rightValue) -> leftValue == rightValue }
+                    newFulfillingNodes.addAll(fulfilling.map { it.first })
+                    newUnfulfillingNodes.addAll(unfulfilling.map { it.first })
+                    if (fulfilling.isEmpty()) {
+                        newUnfulfillingNodes.add(
+                            leftNode
+                        )
+                    } else {
+                        newFulfillingNodes.add(leftNode)
+                    }
                 }
+                ComparisonOperatorName.NEQ -> TODO()
+            }
         }
 
-        return result to (leftProblems + rightProblems + newProblems)
+        return EvaluationResult(
+            newFulfillingNodes,
+            leftUnfulfillingNodes + rightUnfulfillingNodes + newUnfulfillingNodes,
+            leftProblems + rightProblems + newProblems
+        )
     }
 
     private fun evaluateCallConditionComponent(
         callConditionComponent: CallConditionComponent,
         premiseNode: Node? = null
-    ): Pair<Nodes, Problems> {
+    ): EvaluationResult {
         val callNodes = callConditionComponent.op.cpgGetNodes().filterWithDistanceToPremise(premiseNode)
-        return callNodes to Problems()
+        return EvaluationResult(callNodes, emptyList(), Problems())
     }
 
     private fun evaluateContainsConditionComponent(
         containsConditionComponent: ContainsConditionComponent<*, *>,
         premiseNode: Node?
-    ): Pair<Nodes, Problems> {
+    ): EvaluationResult {
         val problems = Problems()
         val nodes = containsConditionComponent.item.cpgGetNodes().filterWithDistanceToPremise(premiseNode)
-        val resultNodes = nodes.filter { node ->
+        val (fulfilling, unfulfilling) = nodes.partition { node ->
             val backendDataItem = node.toBackendDataItem()
             val transformationResult = containsConditionComponent.item.transformation(backendDataItem)
             if (transformationResult.isFailure) {
                 problems.add(
                     transformationResult.getFailureReasonOrNull() ?: "Transformation of $backendDataItem failed",
-                    backendDataItem
+                    node
                 )
                 false
             } else {
@@ -266,7 +305,7 @@ class CpgWheneverEvaluator(premise: ConditionComponent) : WheneverEvaluator(prem
             }
         }
 
-        return resultNodes to Problems()
+        return EvaluationResult(fulfilling, unfulfilling, problems)
     }
 
     private fun Nodes.toNodesToValues(
@@ -280,7 +319,7 @@ class CpgWheneverEvaluator(premise: ConditionComponent) : WheneverEvaluator(prem
             val transformationResult = transformation(backendDataItem)
             val value = transformationResult.getValueOrNull()
             if (value == null) {
-                problems.add(transformationResult.getFailureReasonOrNull() ?: "Value was null", backendDataItem)
+                problems.add(transformationResult.getFailureReasonOrNull() ?: "Value was null", node)
             } else {
                 result[node] = value
             }
@@ -314,16 +353,16 @@ class CpgWheneverEvaluator(premise: ConditionComponent) : WheneverEvaluator(prem
     }
 
     class Problems {
-        private val map: MutableMap<BackendDataItem, String> = mutableMapOf()
+        private val map: MutableMap<Node, String> = mutableMapOf()
 
-        fun add(reason: String, value: BackendDataItem) {
+        fun add(reason: String, value: Node) {
             map[value] = reason
         }
 
-        fun getProblems(): Map<BackendDataItem, String> = map
+        fun getNodes(): Nodes = map.keys
 
-        fun isEmpty(): Boolean {
-            return map.isEmpty()
+        fun isNotEmpty(): Boolean {
+            return map.isNotEmpty()
         }
 
         operator fun plus(other: Problems): Problems {
@@ -332,21 +371,33 @@ class CpgWheneverEvaluator(premise: ConditionComponent) : WheneverEvaluator(prem
         }
 
         override fun toString(): String {
-            return map.toString()
+            return map.mapKeys { (node, _) -> node.toBackendDataItem() }.toString()
         }
     }
 
-    class EvaluationResult() {
-        val fulfillingNodes: MutableList<Node> = mutableListOf()
-        val unfulfillingNodes: MutableList<Node> = mutableListOf()
+    data class EvaluationResult(
+        val fulfillingNodes: MutableList<Node> = mutableListOf(),
+        val unfulfillingNodes: MutableList<Node> = mutableListOf(),
         val problems: Problems = Problems()
+    ) {
+        constructor(
+            fulfillingNodes: Nodes,
+            unfulfillingNodes: Nodes,
+            problems: Problems
+        ) : this(fulfillingNodes.toMutableList(), unfulfillingNodes.toMutableList(), problems)
 
-        constructor(fulfillingNodes: Nodes, unfulfillingNodes: Nodes, problems: Problems): this() {
-            this.fulfillingNodes.addAll(fulfillingNodes)
-            this.unfulfillingNodes.addAll(unfulfillingNodes)
-            this.problems + problems
+        operator fun plus(other: EvaluationResult): EvaluationResult {
+            fulfillingNodes.addAll(other.fulfillingNodes)
+            unfulfillingNodes.addAll(other.unfulfillingNodes)
+            problems + other.problems
+            return this
         }
 
+        fun removeDummyNodes(): EvaluationResult {
+            fulfillingNodes.removeIf { it is DummyNode }
+            unfulfillingNodes.removeIf { it is DummyNode }
+            return this
+        }
     }
 
     class DummyNode : Node()

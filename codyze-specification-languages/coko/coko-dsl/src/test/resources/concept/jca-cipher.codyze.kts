d1b0dd8860fd4cbe266885d538028891a9940e32
Selina Lin <selina.ru-yin.lin@aisec.fraunhofer.de>
2023-09-08 18:22:29 +0200

    implement mode for JCACipherImpl

--- codyze-specification-languages/coko/coko-dsl/src/test/resources/concept/jca-cipher.codyze.kts
+++ codyze-specification-languages/coko/coko-dsl/src/test/resources/concept/jca-cipher.codyze.kts
@@ -1,11 +1,13 @@
 @file:Import("bsi-tr-rules.codyze.kts")
 
+import de.fraunhofer.aisec.codyze.specificationLanguages.coko.core.modelling.BackendDataItem
 import de.fraunhofer.aisec.codyze.specificationLanguages.coko.core.modelling.DataItem
 import de.fraunhofer.aisec.codyze.specificationLanguages.coko.core.modelling.value
 
 class JCACipher {
     fun getInstance(transformation: Any, provider: Any? = null) = op {
-        "javax.crypto.Cipher.getInstance" {
+        // TODO: Change back to "javax.crypto.Cipher.getInstance" when CPG resolves names correctly
+        "Cipher.getInstance" {
             signature(transformation)
             signature(transformation, provider)
         }
@@ -58,30 +60,43 @@ class JCACipherImpl: Bsi_tr_rules_codyze.Cypher {
     val cipher = JCACipher()
 
     override val algo: DataItem<Bsi_tr_rules_codyze.Algorithm>
-        get() = cipher.getInstance(Wildcard, Wildcard).arguments[1] withTransformation { dataItem ->
-            val stringValue = dataItem.value as? String
-            if(dataItem.value == null)
-                TransformationResult.failure("Value of BackendDataItem is null.")
-            else if(stringValue == null) {
-                TransformationResult.failure("Value of BackendDataItem is not a string.")
-            }
-            else {
-                val (algo, _) = stringValue.partition { it == '/' }
-                val algoEnum = Bsi_tr_rules_codyze.Algorithm.values().firstOrNull {  it.name == algo.uppercase() }
-                if(algoEnum == null)
-                    TransformationResult.failure("Could not resolve the string value to an algorithm")
-                else
-                    TransformationResult.success(algoEnum)
+        get() = cipher.getInstance(Wildcard, Wildcard).arguments[0] withTransformation { dataItem ->
+            transformTransformationString(dataItem, 0, "an algorithm") { string ->
+                Bsi_tr_rules_codyze.Algorithm.entries.firstOrNull {  it.name == string.uppercase() }
             }
         }
     override val mode: DataItem<Bsi_tr_rules_codyze.Mode>
-        get() = value(Bsi_tr_rules_codyze.Mode.CBC)
+        get() = cipher.getInstance(Wildcard, Wildcard).arguments[0] withTransformation { dataItem ->
+            transformTransformationString(dataItem, 1, "a mode") { string ->
+                Bsi_tr_rules_codyze.Mode.entries.firstOrNull {  it.name == string.uppercase() }
+            }
+        }
     override val keySize: DataItem<Int>
         get() = value(1)
     override val tagSize: DataItem<Int>
         get() = value(1)
 
 
+    fun <E> transformTransformationString(
+        dataItem: BackendDataItem,
+        index: Int,
+        errorMessage: String,
+        stringToResult: (String) -> E?): TransformationResult<E, String> {
+        val stringValue = dataItem.value as? String
+        return if(dataItem.value == null)
+            TransformationResult.failure("Value of BackendDataItem is null.")
+        else if(stringValue == null) {
+            TransformationResult.failure("Value of BackendDataItem is not a string.")
+        }
+        else {
+            val string = stringValue.split('/')[index]
+            val result = stringToResult(string)
+            if(result == null)
+                TransformationResult.failure("Could not resolve the string value to $errorMessage")
+            else
+                TransformationResult.success(result)
+        }
+    }
 }
 
 

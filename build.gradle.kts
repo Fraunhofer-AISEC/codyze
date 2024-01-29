a385f43f3e8beea4ec15474b6b37d48d1bdc24f3
Robert Haimerl <robert.haimerl@aisec.fraunhofer.de>
2024-01-29 11:59:12 +0100

    replace deprecated setter and getter

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ val projectProps by tasks.registering(WriteProperties::class) {
     description = "Write project properties in a file."
 
     // Set output file to build/project.properties
-    outputFile = file("$buildDir/codyze.properties")
+    destinationFile = file("${layout.buildDirectory}/codyze.properties")
     // Default encoding is ISO-8559-1, here we change it.
     encoding = "UTF-8"
     // Optionally we can specify the header comment.

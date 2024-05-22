020c9391c97762507c9b2595332bc10d7e2bd880
Robert Haimerl <robert.haimerl@aisec.fraunhofer.de>
2024-05-22 11:47:12 +0200

    fix detekt issues

--- codyze-backends/cpg/src/main/kotlin/de/fraunhofer/aisec/codyze/backends/cpg/coko/evaluators/ArgumentEvaluator.kt
+++ codyze-backends/cpg/src/main/kotlin/de/fraunhofer/aisec/codyze/backends/cpg/coko/evaluators/ArgumentEvaluator.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2024, Fraunhofer AISEC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.fraunhofer.aisec.codyze.backends.cpg.coko.evaluators
 
 import de.fraunhofer.aisec.codyze.backends.cpg.coko.CokoCpgBackend
@@ -22,11 +37,13 @@ class ArgumentEvaluator(val targetCall: Op, val argPos: Int, val originCall: Op)
         val findings = mutableListOf<CpgFinding>()
         val targetCalls = targetCall.cpgGetAllNodes()
         for (call in targetCalls) {
-            val arg: VariableDeclaration? = (call.arguments.getOrNull(argPos) as? Reference)?.refersTo as? VariableDeclaration
+            val arg: VariableDeclaration? =
+                (call.arguments.getOrNull(argPos) as? Reference)?.refersTo as? VariableDeclaration
             if (arg in variables) {
                 findings.add(
                     CpgFinding(
-                        message = "Complies with rule: arg $argPos of \"${call.code}\" stems from a call to \"$originCall\"",
+                        message = "Complies with rule: " +
+                            "arg $argPos of \"${call.code}\" stems from a call to \"$originCall\"",
                         kind = Finding.Kind.Pass,
                         node = call,
                         relatedNodes = listOfNotNull(originCalls.firstOrNull { it.tryGetVariableDeclaration() == arg })
@@ -35,7 +52,8 @@ class ArgumentEvaluator(val targetCall: Op, val argPos: Int, val originCall: Op)
             } else {
                 findings.add(
                     CpgFinding(
-                        message = "Violation against rule: arg $argPos of \"${call.code}\" does not stem from a call to \"$originCall\"",
+                        message = "Violation against rule: " +
+                            "arg $argPos of \"${call.code}\" does not stem from a call to \"$originCall\"",
                         kind = Finding.Kind.Fail,
                         node = call,
                         relatedNodes = listOf()

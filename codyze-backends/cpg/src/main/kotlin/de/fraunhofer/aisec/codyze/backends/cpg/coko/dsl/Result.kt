300154292b6855d60d197e40d368156453bdd65b
Robert Haimerl <robert.haimerl@aisec.fraunhofer.de>
2024-07-01 12:35:33 +0200

    add code documentation

--- codyze-backends/cpg/src/main/kotlin/de/fraunhofer/aisec/codyze/backends/cpg/coko/dsl/Result.kt
+++ codyze-backends/cpg/src/main/kotlin/de/fraunhofer/aisec/codyze/backends/cpg/coko/dsl/Result.kt
@@ -2,6 +2,11 @@ package de.fraunhofer.aisec.codyze.backends.cpg.coko.dsl
 
 import de.fraunhofer.aisec.codyze.backends.cpg.coko.dsl.Result.*
 
+/**
+ * A data class that serves as a ternary value for the analysis result.
+ *
+ * OPEN is used where we cannot deduce either VALID or INVALID results because of lack of information.
+ */
 enum class Result {
     VALID,
     INVALID,
@@ -18,14 +23,17 @@ enum class Result {
     }
 }
 
+/** returns VALID if all Results are VALID, otherwise returns OPEN if any result is OPEN, otherwise returns INVALID */
 inline fun <T> Iterable<T>.allResult(predicate: (T) -> Result?): Result {
+    var invalidFlag = false
     for (element in this) {
         if (predicate(element) == OPEN) return OPEN
-        else if (predicate(element) == INVALID) return INVALID
+        else if (predicate(element) == INVALID)invalidFlag = true
     }
-    return VALID
+    return if (invalidFlag) INVALID else VALID
 }
 
+/** returns VALID if any Result is VALID, otherwise returns OPEN if any result is OPEN, otherwise returns INVALID */
 inline fun <T> Iterable<T>.anyResult(predicate: (T) -> Result?): Result {
     var openFlag = false
     for (element in this) {
@@ -35,14 +43,17 @@ inline fun <T> Iterable<T>.anyResult(predicate: (T) -> Result?): Result {
     return if (openFlag) OPEN else INVALID
 }
 
+/** returns VALID if all Results are VALID, otherwise returns OPEN if any result is OPEN, otherwise returns INVALID */
 inline fun <T> Array<T>.allResult(predicate: (T) -> Result?): Result {
+    var invalidFlag = false
     for (element in this) {
         if (predicate(element) == OPEN) return OPEN
-        else if (predicate(element) == INVALID) return INVALID
+        else if (predicate(element) == INVALID)invalidFlag = true
     }
-    return VALID
+    return if (invalidFlag) INVALID else VALID
 }
 
+/** returns VALID if any Result is VALID, otherwise returns OPEN if any result is OPEN, otherwise returns INVALID */
 inline fun <T> Array<T>.anyResult(predicate: (T) -> Result?): Result {
     var openFlag = false
     for (element in this) {
